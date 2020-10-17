e31f353ead40b91fbececc0e89abbade963ef830
timmalstead-cfa <tmalstead@codeforamerica.org>
2020-10-16 17:00:48 -0700

    adds back buttons

--- components/index.ts
+++ components/index.ts
@@ -1,2 +1,3 @@
+export { default as BackButton } from './BackButton'
 export { default as PwaDownloadButton } from './PwaDownloadButton'
 export { default as PwaTags } from './PwaTags'
--- pages/longtermhousing.tsx
+++ pages/longtermhousing.tsx
@@ -3,6 +3,8 @@ import { ReactElement } from 'react'
 import { useRouter } from 'next/router'
 import Head from 'next/head'
 
+import BackButton from '../components/BackButton'
+
 export default (): ReactElement => {
   const { pathname } = useRouter()
   const topic: string = pathname.slice(1)
@@ -13,6 +15,7 @@ export default (): ReactElement => {
         <title>{topic}</title>
       </Head>
       <span>Important information about {topic}</span>
+      <BackButton />
     </>
   )
 }
