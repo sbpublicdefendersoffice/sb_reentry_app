dc4ea613a98bddb78596da5a5453f6355eab8848
timmalstead-cfa <tmalstead@codeforamerica.org>
2021-02-11 14:32:48 -0800

    creates orgpagecontainer to save space in page files. is there a better way to do this?

---
+++ components/OrgPageContainer.tsx
@@ -0,0 +1,42 @@
+import Head from 'next/head'
+import { useState, useEffect } from 'react'
+
+import useSingleRecord from '../hooks/useSingleRecord'
+import { TranslatedRecordResponse } from '../types/records'
+import { fetchRecordsByCategory } from '../services/GET'
+
+import { OrgRecordDisplay, RecordPane } from './'
+
+interface OrgPageContainerProps {
+  category: string
+}
+
+const OrgPageContainer = ({ category }: OrgPageContainerProps) => {
+  const [
+    fetchedRecords,
+    setFetchedRecords,
+  ] = useState<TranslatedRecordResponse | null>(null)
+  const lowCategory: string = category.toLowerCase()
+  useEffect((): void => {
+    fetchRecordsByCategory(lowCategory, setFetchedRecords)
+  }, [])
+  const { singleFetchedRecord } = useSingleRecord()
+
+  return (
+    singleFetchedRecord && (
+      <>
+        <Head>
+          <title>{`Santa Barbara Reentry | ${singleFetchedRecord.name}`}</title>
+        </Head>
+        <RecordPane
+          orgInfo={fetchedRecords}
+          category={category}
+          setRecords={setFetchedRecords}
+        />
+        <OrgRecordDisplay singleFetchedRecord={singleFetchedRecord} />
+      </>
+    )
+  )
+}
+
+export default OrgPageContainer
