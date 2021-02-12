2bbaa52d3e1885a4f438cc1167cbd4b767d73565
timmalstead-cfa <tmalstead@codeforamerica.org>
2021-02-11 16:12:19 -0800

    simplifies head tags

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,7 +7,11 @@ export default class MyDocument extends Document {
         <Head>
           <meta charSet="utf-8" />
           <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-
+          {/* PWA Tags */}
+          <link rel="manifest" href="/manifest.json" />
+          <link rel="apple-touch-icon" href="./images/logo192.png" />
+          <meta name="theme-color" content="#2f4cb8" />
+          {/*  */}
           <link rel="icon" href="./images/heart.svg" type="image/svg+xml" />
           <meta
             name="description"
