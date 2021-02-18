74dcd4f86209bcaa9249f71591acdc0e2fa0635c
timmalstead-cfa <tmalstead@codeforamerica.org>
2021-02-18 13:00:50 -0800

    adds language switcher toggle switch

--- components/LangSwitcher.tsx
+++ components/LangSwitcher.tsx
@@ -8,23 +8,43 @@ const LangSwitcher = () => {
   const { language, setLanguage } = useLanguage()
 
   const [isChecked, setIsChecked] = useState<boolean>(false)
+  const [disabled, setDisabled] = useState<boolean>(false)
   const initialRender = useRef<boolean>(true)
 
   useEffect(() => {
     if (initialRender.current) initialRender.current = false
     else {
+      setDisabled(true)
       if (language === 'english') setLanguage('spanish')
       else setLanguage('english')
+      setTimeout(() => setDisabled(false), 400)
     }
   }, [isChecked])
 
   return (
-    <input
-      className={styles.LangSwitcher}
-      type="checkbox"
-      onClick={() => setIsChecked(!isChecked)}
-      checked={isChecked}
-    />
+    <label className={styles.LangSwitcher} htmlFor="lang-input">
+      <span
+        className={styles.label}
+        style={{ color: isChecked ? 'var(--deselected)' : 'var(--white)' }}
+      >
+        English
+      </span>
+      <input
+        className={styles.input}
+        type="checkbox"
+        id="lang-input"
+        onClick={() => setIsChecked(!isChecked)}
+        checked={isChecked}
+        disabled={disabled}
+      />
+      <span className={styles.slider} />
+      <span
+        className={styles.label}
+        style={{ color: isChecked ? 'var(--white)' : 'var(--deselected)' }}
+      >
+        Spanish
+      </span>
+    </label>
   )
 }
 
