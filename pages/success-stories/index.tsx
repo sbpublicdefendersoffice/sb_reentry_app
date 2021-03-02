d180d9a0c65b2913e7bbc327fcc647d99595c17a
Victor <vsauceda@codeforamerica.org>
2021-03-02 13:22:25 -0800

    taking out cardaction area

--- pages/success-stories/index.tsx
+++ pages/success-stories/index.tsx
@@ -1,15 +1,11 @@
 import Head from 'next/head'
 import { makeStyles } from '@material-ui/core/styles'
 import Card from '@material-ui/core/Card'
-import CardActionArea from '@material-ui/core/CardActionArea'
-import CardActions from '@material-ui/core/CardActions'
 import CardContent from '@material-ui/core/CardContent'
 import CardMedia from '@material-ui/core/CardMedia'
-import Button from '@material-ui/core/Button'
 import Typography from '@material-ui/core/Typography'
 import { CopyHolder, ENGLISH } from '../../types/language'
 import Grid from '@material-ui/core/Grid'
-
 import useLanguage from '../../hooks/useLanguage'
 const useStyles = makeStyles({
   root: {
@@ -79,41 +75,39 @@ const SuccessStories = () => {
           return (
             <Grid item xs={12} sm={2} key={key}>
               <Card className={classes.root}>
-                <CardActionArea>
-                  <CardMedia
-                    className={classes.media}
-                    image={successStory.profilePic}
-                    title={
-                      language === ENGLISH
-                        ? `${successStory.alt} `
-                        : `${successStory.spanishAlt} `
-                    }
-                  />
-                  <CardContent className={classes.cardContent}>
-                    <Typography gutterBottom variant="h5" component="h2">
-                      {successStory.name}
-                    </Typography>
-                    <Typography gutterBottom variant="h5" component="h2">
-                      {language === ENGLISH
-                        ? `${successStory.role} `
-                        : `${successStory.spanishRole} `}
-                    </Typography>
-                    <Typography gutterBottom variant="h5" component="h2">
-                      {language === ENGLISH
-                        ? `${successStory.organization} `
-                        : `${successStory.spanishOrganization} `}
-                    </Typography>
-                    <Typography
-                      variant="body2"
-                      color="textSecondary"
-                      component="p"
-                    >
-                      {language === ENGLISH
-                        ? `${successStory.bio} `
-                        : `${successStory.spanishBio} `}
-                    </Typography>
-                  </CardContent>
-                </CardActionArea>
+                <CardMedia
+                  className={classes.media}
+                  image={successStory.profilePic}
+                  title={
+                    language === ENGLISH
+                      ? `${successStory.alt} `
+                      : `${successStory.spanishAlt} `
+                  }
+                />
+                <CardContent className={classes.cardContent}>
+                  <Typography gutterBottom variant="h5" component="h2">
+                    {successStory.name}
+                  </Typography>
+                  <Typography gutterBottom variant="h5" component="h2">
+                    {language === ENGLISH
+                      ? `${successStory.role} `
+                      : `${successStory.spanishRole} `}
+                  </Typography>
+                  <Typography gutterBottom variant="h5" component="h2">
+                    {language === ENGLISH
+                      ? `${successStory.organization} `
+                      : `${successStory.spanishOrganization} `}
+                  </Typography>
+                  <Typography
+                    variant="body2"
+                    color="textSecondary"
+                    component="p"
+                  >
+                    {language === ENGLISH
+                      ? `${successStory.bio} `
+                      : `${successStory.spanishBio} `}
+                  </Typography>
+                </CardContent>
               </Card>
             </Grid>
           )
