7970fa2abaa957cf672784faa1f25f511715c116
timmalstead-cfa <tmalstead@codeforamerica.org>
2021-03-09 18:13:43 -0800

    seeing if caching is the problem

--- pages/api/airtablerecordsbycategory.ts
+++ pages/api/airtablerecordsbycategory.ts
@@ -19,51 +19,51 @@ const fetchRecordsByCategory = async (
 
     const { category, language } = JSON.parse(req.body)
 
-    const key: string = `${category}_${language}`
-    const cachedData = cacheData.get(key)
+    // const key: string = `${category}_${language}`
+    // const cachedData = cacheData.get(key)
 
-    if (cachedData) {
-      res.json(cachedData)
-    } else {
-      let fetchString: string = `${BASE_URL}/organization?filterByFormula=FIND(%22${category}%22%2Corg_categories)&fields%5B%5D=location_latitude&fields%5B%5D=location_longitude&fields%5B%5D=org_categories`
+    // if (cachedData) {
+    //   res.json(cachedData)
+    // } else {
+    let fetchString: string = `${BASE_URL}/organization?filterByFormula=FIND(%22${category}%22%2Corg_categories)&fields%5B%5D=location_latitude&fields%5B%5D=location_longitude&fields%5B%5D=org_categories`
 
-      if (language === ENGLISH)
-        fetchString += '&fields%5B%5D=org_name&fields%5B%5D=org_tags'
-      else
-        fetchString +=
-          '&fields%5B%5D=org_name_spanish&fields%5B%5D=org_tags_spanish'
+    if (language === ENGLISH)
+      fetchString += '&fields%5B%5D=org_name&fields%5B%5D=org_tags'
+    else
+      fetchString +=
+        '&fields%5B%5D=org_name_spanish&fields%5B%5D=org_tags_spanish'
 
-      const fetchRecords: Response = await fetch(fetchString, OPTIONS_OBJECT)
-      let translatedRecords: TranslatedRecordResponse = await fetchRecords.json()
+    const fetchRecords: Response = await fetch(fetchString, OPTIONS_OBJECT)
+    let translatedRecords: TranslatedRecordResponse = await fetchRecords.json()
 
-      while (translatedRecords.offset) {
-        // Airtable API has a 5 request per second limit. Hence the delay below
-        setTimeout(() => null, 200)
+    while (translatedRecords.offset) {
+      // Airtable API has a 5 request per second limit. Hence the delay below
+      setTimeout(() => null, 200)
 
-        const { offset, records } = translatedRecords
+      const { offset, records } = translatedRecords
 
-        const nextPage: Response = await fetch(
-          `${fetchString}&offset=${offset}`,
-          OPTIONS_OBJECT,
-        )
+      const nextPage: Response = await fetch(
+        `${fetchString}&offset=${offset}`,
+        OPTIONS_OBJECT,
+      )
 
-        const translatedNextPage: TranslatedRecordResponse = await nextPage.json()
+      const translatedNextPage: TranslatedRecordResponse = await nextPage.json()
 
-        const [pageRecords, pageOffset] = [
-          translatedNextPage.records,
-          translatedNextPage.offset,
-        ]
+      const [pageRecords, pageOffset] = [
+        translatedNextPage.records,
+        translatedNextPage.offset,
+      ]
 
-        translatedRecords.records = [...records, ...pageRecords]
+      translatedRecords.records = [...records, ...pageRecords]
 
-        if (pageOffset) translatedRecords.offset = pageOffset
-        else delete translatedRecords.offset
-      }
-
-      cacheData.put(key, translatedRecords, hourInMs)
-
-      res.json(translatedRecords)
+      if (pageOffset) translatedRecords.offset = pageOffset
+      else delete translatedRecords.offset
     }
+
+    // cacheData.put(key, translatedRecords, hourInMs)
+
+    res.json(translatedRecords)
+    // }
   } catch (error) {
     console.error(error)
   }
