4a2854c31e82e46d9b2eeef5d62838652efd618c
timmalstead-cfa <tmalstead@codeforamerica.org>
2021-03-10 19:37:13 -0800

    creates Checkbox ui component, sets up CityFilter component further. nNext up is fecthed data state management and Checkbox styling

--- components/CityFilter.tsx
+++ components/CityFilter.tsx
@@ -1,9 +1,69 @@
+import { ChangeEvent, useState } from 'react'
+
+import Checkbox from '../ui/Checkbox'
+
 import styles from './CityFilter.module.css'
 
-const CityFilter = () => (
-  <div className={styles.CityFilter}>
-    <label className={styles.Label}>South County</label>
-  </div>
-)
+interface CountyVisibilityFilter {
+  southCounty: boolean
+  centralCounty: boolean
+  northCounty: boolean
+}
+
+const checkedState: CountyVisibilityFilter = {
+  southCounty: true,
+  centralCounty: true,
+  northCounty: true,
+}
+
+// TODO: pass down state and set up holding original state and filtering pattern, style checkbox ui component
+
+const CityFilter = () => {
+  const [
+    isRegionVisible,
+    setIsRegionVisible,
+  ] = useState<CountyVisibilityFilter>(checkedState)
+
+  const handleCheck = (e: ChangeEvent<HTMLInputElement>): void =>
+    setIsRegionVisible({
+      ...isRegionVisible,
+      [e.target.id]: !isRegionVisible[e.target.id],
+    })
+
+  return (
+    <form className={styles.CityFilter}>
+      <div className={styles.CheckboxHolder}>
+        <label className={styles.Label} htmlFor="southCounty">
+          South County
+        </label>
+        <Checkbox
+          checked={isRegionVisible.southCounty}
+          onChange={handleCheck}
+          id="southCounty"
+        />
+      </div>
+      <div className={styles.CheckboxHolder}>
+        <label className={styles.Label} htmlFor="centralCounty">
+          Central County
+        </label>
+        <Checkbox
+          checked={isRegionVisible.centralCounty}
+          onChange={handleCheck}
+          id="centralCounty"
+        />
+      </div>
+      <div className={styles.CheckboxHolder}>
+        <label className={styles.Label} htmlFor="northCounty">
+          North County
+        </label>
+        <Checkbox
+          checked={isRegionVisible.northCounty}
+          onChange={handleCheck}
+          id="northCounty"
+        />
+      </div>
+    </form>
+  )
+}
 
 export default CityFilter
