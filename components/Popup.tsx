0d69779a885f0842f2a5ef61a9734fc8db44d377
timmalstead-cfa <tmalstead@codeforamerica.org>
2021-03-15 18:09:35 -0700

    edits Popup component so that if there is a collision with the right edge of the client, it is moved to avoid it

--- components/Popup.tsx
+++ components/Popup.tsx
@@ -1,20 +1,34 @@
-import { ReactNode } from 'react'
+import { ReactNode, useEffect, useRef, useState, MutableRefObject } from 'react'
 
 import { PopupInfo } from '../types/maps'
-
 import styles from './Popup.module.css'
 
 interface PopupProps extends PopupInfo {
   children: ReactNode
 }
 
-const Popup = ({ children, clientX, clientY }: PopupProps) => (
-  <aside
-    style={{ left: `${clientX + 10}px`, top: `${clientY + 10}px` }}
-    className={styles.Popup}
-  >
-    {children}
-  </aside>
-)
+const Popup = ({ children, clientX, clientY }: PopupProps) => {
+  const [calcXvalue, setCalcXvalue] = useState<number | null>(null)
+  const ref: MutableRefObject<HTMLElement> | null = useRef(null)
+
+  useEffect(() => {
+    if (ref.current && clientX) {
+      const { width } = ref.current.getClientRects()[0]
+      const widerThenWindow: boolean = clientX + width > innerWidth
+      if (widerThenWindow) setCalcXvalue(clientX - width)
+      else setCalcXvalue(clientX)
+    }
+  }, [])
+
+  return (
+    <aside
+      ref={ref}
+      style={{ left: `${calcXvalue + 10}px`, top: `${clientY + 10}px` }}
+      className={styles.Popup}
+    >
+      {children}
+    </aside>
+  )
+}
 
 export default Popup
