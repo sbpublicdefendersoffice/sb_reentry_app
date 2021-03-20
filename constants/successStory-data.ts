80a45ee995ad41ccc94356c64158bb7810a15438
Victor <vsauceda@codeforamerica.org>
2021-03-19 18:12:28 -0700

    storing data elsewhere

---
+++ constants/successStory-data.ts
@@ -0,0 +1,52 @@
+import { CopyHolder } from '../types/language'
+import { SuccessStory } from '../types/successStory'
+export const copy: CopyHolder = {
+  english: {
+    title: 'Success Stories',
+  },
+
+  spanish: {
+    title: 'Historias de éxito',
+  },
+}
+export const ArturoSuccessStory: SuccessStory = {
+  english: {
+    name: 'Arturo "Cheech" Raygoza',
+
+    role: 'President of BIG E',
+    organization: 'Alan Hancock College',
+    profilePic: 'https://i.ibb.co/R0mMtMv/BIO-Picture.jpg',
+    alt: 'picture of Arturo Raygoza',
+    bio:
+      'I am a formerly incarcerated student who after thirteen years in and out of prison, graduated with my AS degree in Automotive Technology. I recently switched my career to Sociology with Transfer and plan on attending either UCSB or Berkeley. I’m currently President of the B.I.G.E.(Beyond Incarceration Greater Education) Club which helps formerly incarcerated individuals enroll in college and lower recidivism. I also work for Allan Hancock College’s Beyond Barriers program as a student mentor, giving back to my community what was freely given to me.',
+  },
+  spanish: {
+    name: 'Arturo "Cheech" Raygoza',
+    profilePic: 'https://i.ibb.co/R0mMtMv/BIO-Picture.jpg',
+    role: 'Presidente de BIG E',
+    organization: 'Universidad Allan Hancock',
+    alt: 'foto de Arturo Raygoza',
+    bio:
+      'Soy un estudiante anteriormente encarcelado que después de trece años dentro y fuera de prisión, se graduó con mi título de AS en Tecnología Automotriz. Recientemente cambié mi carrera a Sociología con Transferencia y planeo asistir a UCSB o Berkeley. Actualmente soy presidente del Club B.I.G.E. (Beyond Incarceration Greater Education), que ayuda a las personas anteriormente encarceladas a inscribirse en la universidad y reducir la reincidencia. También trabajo para el programa Beyond Barriers de Allan Hancock College como mentor estudiantil, devolviendo a mi comunidad lo que me fue dado gratuitamente.',
+  },
+}
+export const LisaSuccessStory: SuccessStory = {
+  english: {
+    name: 'Lisandra Barrera-Rising',
+
+    role: 'Underground Scholars Santa Barbara Ambassador',
+    organization: 'Santa Barbara City College',
+    profilePic: 'https://i.ibb.co/kK1Kt7s/lisarising.jpg',
+    alt: 'picture of Lisa Barrera-Rising',
+    bio: `My name is Lisandra "Lisa" Barrera Rising. I currently attend Santa Barbara City College in pursuit to attaining my major in Sociology. I have proudly overcome the systemic oppression of the injustice system by beating a 13 year opiate addiction, losing and regaining parental custody over my daughter while going in and out of jail. Today, I have found a passion in my academic journey and want to support and encourage anybody else going through the same challenges I once experienced. Anything is possible through community and valuable resources! Let's all stand to make a change and prove society we can persevere despite our pasts mistakes.`,
+  },
+  spanish: {
+    name: 'Lisandra Barrera-Rising',
+    profilePic: 'https://i.ibb.co/kK1Kt7s/lisarising.jpg',
+    role:
+      'Embajador de Underground Scholars en Santa Bárbara spanishOrg: Colegio de la ciudad de Santa Bárbara',
+    organization: 'Colegio de la ciudad de Santa Bárbara',
+    alt: 'foto de Lisandra "Lisa" Barrera-Rising',
+    bio: `Mi nombre es Lisandra "Lisa" Barrera Rising. Actualmente asisto a Santa Barbara City College en pos de obtener mi especialización en Sociología. He superado con orgullo la opresión sistémica del sistema de injusticia superando una adicción a los opiáceos durante 13 años, perdiendo y recuperando la custodia de los padres sobre mi hija mientras entraba y salía de la cárcel. Hoy, he encontrado una pasión en mi viaje académico y quiero apoyar y alentar a cualquier otra persona que atraviese los mismos desafíos que una vez experimenté. ¡Todo es posible a través de la comunidad y recursos valiosos! Estemos todos para hacer un cambio y demostrarle a la sociedad que podemos perseverar a pesar de los errores del pasado.`,
+  },
+}
