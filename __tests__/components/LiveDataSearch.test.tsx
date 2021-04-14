b722c0c91dd1f79b82c0507663a6fe23f2f3a293
timmalstead-cfa <tmalstead@codeforamerica.org>
2021-04-13 18:14:50 -0700

    adds tests for LiveDataSearch, coverage is not great but I expect it will be better once I test some user flows that involve search.

---
+++ __tests__/components/LiveDataSearch.test.tsx
@@ -0,0 +1,47 @@
+import { renderWithAllContext } from '../__helpers__/contexts'
+
+import { searchCopy, SPANISH } from '../../constants/'
+import LiveDataSearch from '../../components/LiveDataSearch'
+import { fireEvent, waitFor } from '@testing-library/dom'
+
+describe('<LiveDataSearch />', () => {
+  it('renders correctly', () => {
+    const { getByRole } = renderWithAllContext(<LiveDataSearch />)
+
+    const dataSearchNode: HTMLElement = getByRole('searchbox')
+
+    expect(dataSearchNode).toBeInTheDocument()
+  })
+
+  it('renders english contect correctly', async () => {
+    const { getByRole } = renderWithAllContext(<LiveDataSearch />)
+
+    const { search, tooltip } = searchCopy.english
+
+    const [inputNode, tooltipNode] = [getByRole('search'), getByRole('tooltip')]
+
+    fireEvent.mouseOver(tooltipNode)
+
+    const popUpNode: HTMLElement = await waitFor(() => getByRole('note'))
+
+    expect(inputNode).toHaveAttribute('placeholder', `${search}...`)
+    expect(popUpNode).toHaveTextContent(tooltip)
+  })
+
+  it('renders spanish contect correctly', async () => {
+    const { getByRole } = renderWithAllContext(<LiveDataSearch />, {
+      language: SPANISH,
+    })
+
+    const { search, tooltip } = searchCopy.spanish
+
+    const [inputNode, tooltipNode] = [getByRole('search'), getByRole('tooltip')]
+
+    fireEvent.mouseOver(tooltipNode)
+
+    const popUpNode: HTMLElement = await waitFor(() => getByRole('note'))
+
+    expect(inputNode).toHaveAttribute('placeholder', `${search}...`)
+    expect(popUpNode).toHaveTextContent(tooltip)
+  })
+})
