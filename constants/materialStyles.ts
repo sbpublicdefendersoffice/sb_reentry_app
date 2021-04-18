4c709b017447352b2940e1c3c380bef3cde1b7ca
Victor <vsauceda@codeforamerica.org>
2021-04-18 12:32:29 -0700

    constants

---
+++ constants/materialStyles.ts
@@ -0,0 +1,166 @@
+import { makeStyles, createStyles, Theme } from '@material-ui/core/styles'
+
+export const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      width: '100%',
+      height: '100%',
+    },
+    heading: {
+      fontSize: '2rem',
+    },
+    paper: {
+      padding: theme.spacing(2),
+      textAlign: 'center',
+      color: theme.palette.text.secondary,
+      minHeight: '10rem',
+    },
+    paperModal: {
+      padding: theme.spacing(2),
+      textAlign: 'center',
+      marginTop: '2rem',
+      margin: 'auto',
+      width: '80%',
+      color: theme.palette.text.secondary,
+      height: '40rem',
+      overflow: 'scroll',
+      '@media (max-width: 500px)': {
+        width: '100%',
+        marginLeft: '0rem',
+      },
+    },
+    fontSize: {
+      fontSize: '1.6rem',
+    },
+    ulist: {
+      listStyle: 'square',
+      fontSize: '1.6rem',
+    },
+    h4Style: {
+      marginBottom: '2rem',
+      fontSize: '2rem',
+    },
+    listItem: {
+      marginBottom: '2rem',
+      marginLeft: '2rem',
+    },
+    listItemText: {
+      fontSize: '1.6rem',
+      textAlign: 'center',
+      justifyContent: 'center',
+    },
+    centerFlex: {
+      fontSize: '1.6rem',
+      textAlign: 'center',
+      justifyContent: 'center',
+    },
+    centerCard: {
+      marginBottom: '2rem',
+      marginTop: '2rem',
+      marginLeft: '2rem',
+      width: 'auto',
+      textAlign: 'center',
+      justifyContent: 'center',
+    },
+    pageDescription: {
+      marginTop: '2rem',
+      marginBottom: '2rem',
+      padding: '2rem',
+      fontSize: '1.8rem',
+      textAlign: 'justify',
+    },
+    aBlock: {
+      display: 'block !important',
+      backgroundColor: 'none',
+      margin: '1rem 1rem 1rem 1rem',
+      fontSize: '1.8rem',
+      minWidth: '40%',
+      textDecoration: 'none !important',
+    },
+    paperCard: {
+      padding: theme.spacing(2),
+      textAlign: 'center',
+      color: theme.palette.text.secondary,
+      minHeight: '22rem',
+    },
+
+    media: {
+      height: '30rem',
+    },
+    card: {
+      width: '40rem',
+      height: '100%',
+      display: 'block',
+    },
+
+    cardContent: {
+      textAlign: 'center',
+      marginTop: '3rem',
+      overflow: 'auto',
+      pointerEvents: 'auto',
+    },
+    flyerRoot: {
+      maxWidth: '90%',
+      marginLeft: '2rem',
+      marginRight: '2rem',
+      height: '18rem',
+      '@media (max-width: 700px)': {
+        maxWidth: '100%',
+        marginLeft: '0rem',
+        marginRight: '0rem',
+        marginTop: '2rem',
+      },
+    },
+
+    flyerCardContent: {
+      textAlign: 'center',
+      width: '100%',
+
+      height: '18rem !important',
+      justifyContent: 'center',
+      backgroundColor: '#13395E',
+      color: 'white',
+    },
+    flyerMedia: {
+      width: '12rem',
+      height: '13.5rem',
+      overflow: 'auto',
+    },
+    flyerPaper: {
+      backgroundColor: 'white',
+      width: '50%',
+      marginLeft: '15rem',
+      height: '100%',
+      position: 'fixed',
+      top: '50%',
+      transform: 'translateY(-50%)',
+      padding: '1rem',
+      borderColor: 'white !important',
+      overflow: 'auto',
+      maxHeight: '70rem',
+      '@media (max-width: 500px)': {
+        width: '100%',
+        marginLeft: '0rem',
+      },
+    },
+
+    flyerIcons: {
+      position: 'fixed',
+      color: '#13395e',
+      margin: 'auto',
+      fontSize: '3rem',
+    },
+    flyerImage: {
+      position: 'fixed',
+      width: ' 50%',
+      marginLeft: '30rem',
+
+      fontSize: '3rem',
+      '@media (max-width: 500px)': {
+        marginTop: '10rem',
+        width: '100%',
+        marginLeft: '-3rem',
+      },
+    },
+  }),
+)
---
+++ constants/rights-data.ts
@@ -0,0 +1,131 @@
+import { FlyerPDF } from '../types/flyerPDF'
+import { CopyHolder } from '../types/language'
+export const copy: CopyHolder = {
+  english: {
+    title: 'Know Your Rights',
+    download: 'Download',
+    view: 'view',
+  },
+
+  spanish: {
+    title: 'Conoce tus derechos',
+    download: 'descargar',
+    view: 'vista',
+  },
+}
+export const PoliceInteractionBlackAndBrownPDF: FlyerPDF = {
+  english: {
+    name: 'Police Interaction for Black and Brown People',
+    organization: 'ACLU',
+    flyerPDF: '/images/blackandbrownenglish.jpg',
+    thumb: '/images/blackandbrownspanish-thumb.jpg',
+  },
+  spanish: {
+    name: 'Interacción policial para personas negras y morenas',
+    organization: 'ACLU',
+    flyerPDF: '/images/blackandbrownspanish.jpg',
+    thumb: '/images/blackandbrownspanish-thumb.jpg',
+  },
+}
+export const QuestionedByPolicePDF: FlyerPDF = {
+  english: {
+    name:
+      'What to do if questioned by Police, FBI, Custom Agents or Immigration Officers',
+    organization: 'ACLU',
+    flyerPDF: '/images/questionedenglish.jpg',
+    thumb: '/images/questionedenglish-thumb.jpg',
+  },
+  spanish: {
+    name:
+      'Qué hacer si es interrogado por la policía, el FBI, agentes de aduanas o oficiales de inmigración',
+    organization: 'ACLU',
+    flyerPDF: '/images/questionedspanish.jpg',
+    thumb: '/images/questionedspanish-thumb.jpg',
+  },
+}
+export const PoliceInteractionPDF: FlyerPDF = {
+  english: {
+    name: 'Know your rights: Police Interactions ',
+    organization: 'ACLU',
+    flyerPDF: '/images/policeinteractionsenglish.jpg',
+    thumb: '/images/policeinteractionsenglish-thumb.jpg',
+  },
+  spanish: {
+    name: 'Conozca sus derechos: Interacciones policiales',
+    organization: 'ACLU',
+    flyerPDF: '/images/policeinteractionsspanish.jpg',
+    thumb: '/images/policeinteractionsspanish-thumb.jpg',
+  },
+}
+export const TrustActPDF: FlyerPDF = {
+  english: {
+    name: 'Trust ACT ',
+    organization: 'ACLU',
+    flyerPDF: '/images/trust.jpg',
+    thumb: '/images/trust-thumb.jpg',
+  },
+  spanish: {
+    name: 'Trust ACT',
+    organization: 'ACLU',
+    flyerPDF: '/images/trust.jpg',
+    thumb: '/images/trust-thumb.jpg',
+  },
+}
+export const TrainsBusesPDF: FlyerPDF = {
+  english: {
+    name: 'Know your rights on trains and buses ',
+    organization: 'ACLU',
+    flyerPDF: '/images/trainsandbusesenglish.jpg',
+    thumb: '/images/trainsandbusesenglish-thumb.jpg',
+  },
+  spanish: {
+    name: 'Conozca sus derechos en trenes y autobuses',
+    organization: 'ACLU',
+    flyerPDF: '/images/trainsandbusesspanish.jpg',
+    thumb: '/images/trainsandbusesspanish-thumb.jpg',
+  },
+}
+export const TrainsBusesImmigrationPDF: FlyerPDF = {
+  english: {
+    name:
+      'Know your rights on trains and buses when it comes to immagration agents ',
+    organization: 'ACLU',
+    flyerPDF: '/images/trainsandbusesimmigrationenglish.jpg',
+    thumb: '/images/trainsandbusesimmigrationenglish-thumb.jpg',
+  },
+  spanish: {
+    name:
+      'Conozca sus derechos en trenes y autobuses cuando se trata de agentes de inmigración',
+    organization: 'ACLU',
+    flyerPDF: '/images/trainsandbusesimmigrationspanish.jpg',
+    thumb: '/images/trainsandbusesimmigrationspanish-thumb.jpg',
+  },
+}
+export const SB1421PDF: FlyerPDF = {
+  english: {
+    name: 'SB 1421 ',
+    organization: 'ACLU',
+    flyerPDF: '/images/sb1421.jpg',
+    thumb: '/images/sb1421-thumb.jpg',
+  },
+  spanish: {
+    name: 'SB 1421',
+    organization: 'ACLU',
+    flyerPDF: '/images/sb1421.jpg',
+    thumb: '/images/sb1421-thumb.jpg',
+  },
+}
+export const SB54PDF: FlyerPDF = {
+  english: {
+    name: 'SB 54 ',
+    organization: 'ACLU',
+    flyerPDF: '/images/sb54.jpg',
+    thumb: '/images/sb54-thumb.jpg',
+  },
+  spanish: {
+    name: 'SB 1421',
+    organization: 'ACLU',
+    flyerPDF: '/images/sb54.jpg',
+    thumb: '/images/sb54-thumb.jpg',
+  },
+}
