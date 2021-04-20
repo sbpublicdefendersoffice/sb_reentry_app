e85ca1a8dffe05fde7d02593c7f22f3c3abb38bb
timmalstead-cfa <tmalstead@codeforamerica.org>
2021-04-20 14:03:23 -0700

    adds slightly more complex integration test for checklist page

--- __tests__/pages/checklist.test.tsx
+++ __tests__/pages/checklist.test.tsx
@@ -1,5 +1,17 @@
+import { useRouter } from 'next/router'
 import { getPage } from 'next-page-tester'
-import { waitFor, screen } from '@testing-library/react'
+import { waitFor, screen, fireEvent } from '@testing-library/react'
+
+jest.mock('next/router', () => ({
+  useRouter: jest.fn(),
+}))
+
+const push = jest.fn()
+
+// @ts-ignore
+useRouter.mockImplementation(() => ({ push }))
+
+const expanded: RegExp = /expanded/
 
 describe('checklist page', () => {
   it('renders checklist page', async () => {
@@ -15,6 +27,23 @@ describe('checklist page', () => {
       screen.getByTestId('end_of_doc'),
     )
 
+    const accordions: HTMLElement[] = await waitFor(() =>
+      screen.getAllByTestId('accordion'),
+    )
+
+    accordions.forEach((node: HTMLElement) => fireEvent.click(node))
+
     expect(doc).toBeInTheDocument()
+    accordions.forEach((node: HTMLElement) =>
+      expect(node).toHaveAttribute('class', expect.stringMatching(expanded)),
+    )
+
+    const link: HTMLElement = await waitFor(() =>
+      screen.getByTestId('test-link'),
+    )
+
+    fireEvent.click(link)
+
+    expect(push).toHaveBeenCalledWith('/search', 'search?query=pantry')
   })
 })
