de0010268cb1f48813f655cb9f113b7b02b8418b
Victor <vsauceda@codeforamerica.org>
2021-05-04 13:18:30 -0700

    adding privacypolicy type

---
+++ types/privacypolicy.ts
@@ -0,0 +1,37 @@
+export interface PrivacyPolicy {
+  english: Item
+  spanish: Item
+}
+interface Item {
+  intro: string
+  twoelevenlink: string
+  twoeleven: string
+  learnMore: string
+  overview: string
+  overviewDescription: string
+  weCollect: string
+  weCollectDescription: string
+  weCollectLineOne: string
+  weCollectLineTwo: string
+  weCollectLineThree: string
+  weCollectLineFour: string
+  weCollectLineFive: string
+  weCollectClosing: string
+  weUse: string
+  weUseDescription: string
+  weShare: string
+  weShareDescription: string
+  sbpdHref: string
+  sbpd: string
+  weDisclose: string
+  howWeProtect: string
+  howWeProtectDescription: string
+  changes: string
+  changesDescription: string
+  effectiveDate: string
+  effectiveDateDescription: string
+  questions: string
+  questionsDescription: string
+  contactEmail: string
+  doOurBest: string
+}
