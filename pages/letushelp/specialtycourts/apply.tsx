290d9cf69cf6f1b94ea005d37d1bccc0d3996282
timmalstead-cfa <tmalstead@codeforamerica.org>
2021-05-05 12:58:59 -0700

    hooks up apply for specialty courts to ApplyForService component

--- pages/letushelp/specialtycourts/apply.tsx
+++ pages/letushelp/specialtycourts/apply.tsx
@@ -1,2 +1,27 @@
-const ApplyForSpecialtyCourtsLanding = () => <span>apply to be special!</span>
+import ApplyForService from '../../../components/ApplyForService'
+import { CopyHolder } from '../../../types/language'
+import useLanguage from '../../../hooks/useLanguage'
+
+export const copy: CopyHolder = {
+  english: {
+    apply:
+      "Got it! The first step toward qualifying for Specialty Courts is to apply with the Public Defender's office.",
+    learn: 'Want to learn more about Specialty Courts?',
+  },
+  spanish: {
+    apply:
+      '¡Entendido! El primer paso para calificar para los Tribunales de Especialidades es presentar una solicitud en la oficina del Defensor Público.',
+    learn:
+      '¿Quiere obtener más información sobre los tribunales especializados?',
+  },
+}
+
+export const route: string = 'specialtycourts'
+
+const ApplyForSpecialtyCourtsLanding = () => {
+  const { language } = useLanguage()
+  const parentCopy = copy[language]
+
+  return <ApplyForService parentCopy={parentCopy} route={route} />
+}
 export default ApplyForSpecialtyCourtsLanding
