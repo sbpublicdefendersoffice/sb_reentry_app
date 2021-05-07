6d418f002bd017f08ae930254d06825ab7558faf
Victor <vsauceda@codeforamerica.org>
2021-05-07 12:30:49 -0700

    moving data and translations

---
+++ constants/aboutus-data.ts
@@ -0,0 +1,25 @@
+import { CopyHolder } from '../types/language'
+export const copy: CopyHolder = {
+  english: {
+    title: 'About Us',
+    provide: 'Provide Accurate Information',
+    support: 'Support',
+    community: 'Community Outreach',
+    howWeDid: 'How We did this',
+    ourProcess: 'Our Process at Code for America',
+    partnered: `Code for America partnered with the Santa Barbara County Public Defender's office to develop a re-entry application for individuals who will be returning to Santa Barbara after incarceration. Jeanmarie Levy, Timothy Malstead and Victor Sauceda were assigned to developing this application in October 2020.`,
+    building: `Building this application with a human-centered design approach allowed them to better understand the needs of the justice impacted. After many interviews with justice impacted individuals and community based organizations they noticed a need for a centralized location of resources. After validating over 1000 resources available across the platform to ensure accuracy they hope that you are able to benefit from the use of this resource application. Good luck in your re-entry process, Santa Barbara County is rooting for you!`,
+  },
+
+  spanish: {
+    title: 'Sobre nosotros`',
+    provide: 'Proporcione información precisa',
+    support: 'Apoyo',
+    community: 'Alcance comunitario',
+    howWeDid: 'Como lo hicimos',
+    ourProcess: 'Nuestro proceso en Code for America',
+    partnered:
+      'Code for America se asoció con la oficina del Defensor Público del Condado de Santa Bárbara para desarrollar una solicitud de reingreso para las personas que regresarán a Santa Bárbara después del encarcelamiento. Jeanmarie Levy, Timothy Malstead y Victor Sauceda fueron asignados a desarrollar esta aplicación en octubre de 2020.',
+    building: `La construcción de esta aplicación con un enfoque de diseño centrado en el ser humano les permitió comprender mejor las necesidades de la justicia afectada. Después de muchas entrevistas con personas afectadas por la justicia y organizaciones comunitarias, notaron la necesidad de una ubicación centralizada de los recursos. Después de validar más de 1000 recursos disponibles en la plataforma para garantizar la precisión, esperan que pueda beneficiarse del uso de esta aplicación de recursos. ¡Buena suerte en su proceso de reingreso, el condado de Santa Bárbara lo apoya!`,
+  },
+}
