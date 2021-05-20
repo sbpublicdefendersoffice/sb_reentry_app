3e1c8c0bfc3a7972f6f23f74350e7cbbef624118
Victor <vsauceda@codeforamerica.org>
2021-05-20 12:20:53 -0700

    adding resources for women

--- pages/letushelp/resourcesforwomen/index.tsx
+++ pages/letushelp/resourcesforwomen/index.tsx
@@ -1,7 +1,47 @@
+import {
+  PictureWithOval,
+  LetUsHelpHeading,
+  LetUsHelpCardLayout,
+} from '../../../components'
+import useLanguage from '../../../hooks/useLanguage'
+import { CopyHolder } from '../../../types/language'
+import { womensResources } from '../../../constants/cards'
+import { Title, Paragraph } from '../../../ui'
+
+const copy: CopyHolder = {
+  english: {
+    title: 'Resources for Cis/Trans Women',
+    explainer:
+      'Justice involved cisgender women, trans women and gender nonconforming individuals are often overlooked. This can greatly impact their ability to reenter successfully. Below you can find a list of resources that can provide support.',
+    heading: 'Featured Resources',
+  },
+  spanish: {
+    title: 'Recursos para mujeres Cis/Trans',
+    explainer:
+      'La justicia involucra a mujeres cisgénero, mujeres trans e individuos no conformes con el género a menudo se pasa por alto. Esto puede afectar en gran medida su capacidad para reingresar con éxito. A continuación, puede encontrar una lista de recursos que pueden brindar apoyo.',
+    heading: 'Recursos destacados',
+  },
+}
+
 const ResourcesForWomenLanding = () => {
-  return <span>resources...for the ladies</span>
+  const { language } = useLanguage()
+  const { title, explainer, heading } = copy[language]
+
+  return (
+    <>
+      <div style={{ width: '100%', display: 'flex' }}>
+        <PictureWithOval
+          color="var(--peri)"
+          pic="resourcesforwomen_placeholder.png"
+        />
+        <LetUsHelpHeading>
+          <Title>{title}</Title>
+          <Paragraph size="med-text">{explainer}</Paragraph>
+        </LetUsHelpHeading>
+      </div>
+      <LetUsHelpCardLayout heading={heading} cards={womensResources} />
+    </>
+  )
 }
 
 export default ResourcesForWomenLanding
-
-// resourcesforwomen_placeholder.png
