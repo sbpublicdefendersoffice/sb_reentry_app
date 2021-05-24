c126ac01713eaab3b9f37a44e7fdaaa8d9c69c41
timmalstead-cfa <tmalstead@codeforamerica.org>
2021-05-24 12:18:03 -0700

    begins redoing let us help cta

--- pages/index.tsx
+++ pages/index.tsx
@@ -24,6 +24,10 @@ const copy: CopyHolder = {
     explainer:
       'On ThriveSBC you will find a variety of resources to help you or a loved one with re-entry after a jail or prison stay. We know this is a tough task, and Santa Barbara County is here to help!',
     buttonText: 'See Our Resources To Get Started',
+    title2: 'Not sure where to start?',
+    explainer2:
+      'So much information can be overwhelming, but there are many routes to take to get help.',
+    buttonText2: 'Let us help',
   },
   spanish: {
     title:
@@ -31,15 +35,21 @@ const copy: CopyHolder = {
     explainer:
       'En ThriveSBC encontrará una variedad de recursos para ayudarlo a usted oa un ser querido con el reingreso después de una estancia en la cárcel o prisión. ¡Sabemos que esta es una tarea difícil y el condado de Santa Bárbara está aquí para ayudar!',
     buttonText: 'Vea nuestros recursos para comenzar',
+    title2: '¿No estás seguro por dónde empezar?',
+    explainer2:
+      'Tanta información puede ser abrumadora, pero hay muchas rutas que puede tomar para obtener ayuda.',
+    buttonText2: 'Ayudemos',
   },
 }
 
 export const url: string = '/checklist'
+export const url2: string = '/letushelp'
 
 const Home = () => {
   const { push } = useRouter()
   const { language } = useLanguage()
-  const { title, explainer, buttonText } = copy[language]
+  const { title, explainer, buttonText, title2, explainer2, buttonText2 } =
+    copy[language]
 
   return (
     <>
@@ -57,8 +67,18 @@ const Home = () => {
       </div>
       <ResourceHub />
       <TopThreeCta />
-      {/* <LetUsHelpCta /> */}
+      <div style={{ ...flexFullWidth, textAlign: 'end' }}>
+        <LetUsHelpHeading>
+          <Title>{title2}</Title>
+          <Paragraph style={{ marginBottom: 'var(--margin-lg)' }}>
+            {explainer2}
+          </Paragraph>
+          <Button onClick={() => push(url2, url2)}>{buttonText2}</Button>
+        </LetUsHelpHeading>
+        <PictureWithOval color="green" pic="wheretostart_placeholder.png" />
+      </div>
       <PwaDownload />
+      {/* <LetUsHelpCta /> */}
       {/* <MostUsedResourcesCta /> */}
       {/* <PRRCcta /> */}
       {/* <AccessLineCta /> */}
