f40545c5ec9f7ee8cc719a458b26a2bd3822bbe8
Victor <vsauceda@codeforamerica.org>
2021-05-26 17:22:29 -0700

    restructuring data

--- constants/faq-data.ts
+++ constants/faq-data.ts
@@ -3,13 +3,15 @@ export const faqCopy: CopyHolder = {
   english: {
     title: 'Frequently Asked Questions',
     description:
-      'Here are some Frequently Asked Questions that can be useful during the re-entry process in Santa Barbara County. Click on a question below to learn more',
+      'Here are some Frequently Asked Questions that can be useful during the re-entry process in Santa Barbara County. Click on a question below to learn more.',
+    clickHere: 'clicking here.',
   },
 
   spanish: {
     title: 'Preguntas frecuentes',
     description:
       'Aquí hay algunas preguntas frecuentes que pueden ser útiles durante el proceso de reingreso en el condado de Santa Bárbara. Haga clic en una de las preguntas siguientes para obtener más información.',
+    clickHere: 'haciendo clic aquí.',
   },
 }
 export const whereToStart: CopyHolder = {
@@ -17,37 +19,38 @@ export const whereToStart: CopyHolder = {
     listItem:
       'I am releasing and would like to know where is a good place to start?',
     description:
-      'A good place to start will be to click here You can click here',
+      'A good place to start is our "Resource Checklist" which you can access by ',
+
     description2:
-      'You can also click navigate to the “Not sure where to start?” by clicking here to support you with key information.',
-    href1: 'https://www.thrivesbc.com/checklist',
-    href2: 'https://www.thrivesbc.com/letushelp',
+      'You can also navigate to the “Not sure where to start?” to help support you with key information by ',
+    nextLink: '/checklist',
+    nextLinkTwo: '/letushelp',
   },
   spanish: {
     listItem:
       'Estoy lanzando y me gustaría saber dónde es un buen lugar para comenzar. ',
-    description:
-      'Un buen lugar para comenzar será hacer clic aquí. Puede hacer clic aquí.',
-    description2: `también puede hacer clic en navegar a "¿No está seguro de por dónde empezar?" haciendo clic aquí para brindarle información clave.`,
+    description: `Un buen lugar para comenzar es nuestra "Lista de verificación de recursos", a la que puede acceder  `,
+    description2: `
+    También puede navegar a "¿No está seguro de por dónde empezar?" para ayudarlo con información clave al `,
 
-    href1: 'https://www.thrivesbc.com/checklist',
-    href2: 'https://www.thrivesbc.com/letushelp',
+    nextLink: '/checklist',
+    nextLinkTwo: '/letushelp',
   },
 }
 export const probation: CopyHolder = {
   english: {
     listItem: `I have just been released but don't know how to contact my probation/parole officer?`,
     description:
-      'Click here. Once on the page click on the “Report to your probation or parole officer to find more information.',
+      'Check out our “Report to your probation or parole officer to find more information" you can access this by ',
 
-    href1: 'https://www.thrivesbc.com/checklist',
+    nextLink: '/checklist',
   },
   spanish: {
     listItem:
       '¿Acabo de ser liberado pero no sé cómo contactar a mi oficial de libertad condicional?',
     description:
-      'Haga clic aquí. Una vez en la página, haga clic en “Informe a su oficial de libertad condicional o de libertad condicional para encontrar más información.',
-    href1: 'https://www.thrivesbc.com/checklist',
+      'Consulte nuestro "Informe a su oficial de libertad condicional o libertad condicional para encontrar más información". Puede acceder a esto por',
+    nextLink: '/checklist',
   },
 }
 export const transferProbation: CopyHolder = {
@@ -80,26 +83,26 @@ export const treatment: CopyHolder = {
   english: {
     listItem: ` I was Court-Ordered to enter into a treatment program, what should I do?`,
     description:
-      '"Court-ordered" treatment includes DUI or DUI programs, drug diversion programs or PC 1000, Proposition 36 or the Crime and Substance Abuse Prevention Act (SACPA), and the Criminal Court. Drugs Those directed by the Court to obtain treatment for any of these violations or programs should contact their parole or probation officer or assigned attorney to discuss which program they may be required to attend. If you have questions about DUI, PC1000 (Drug Diversion), Drug Courts, or SACPA (Prop 36), you can contact the Behavioral Wellness 24-Hour Toll Free Access Line (888) 868-1649.',
+      '"Court-ordered" treatment includes DUI or DUI programs, drug diversion programs or PC 1000, Proposition 36 or the Crime and Substance Abuse Prevention Act (SACPA), and the Criminal Court. Drugs Those directed by the Court to obtain treatment for any of these violations or programs should contact their parole or probation officer or assigned attorney to discuss which program they may be required to attend. If you have questions about DUI, PC1000 (Drug Diversion), Drug Courts, or SACPA (Prop 36), you can contact the Behavioral Wellness 24-Hour Toll Free Access Line (888) 868-1649 or .',
+    href1: 'tel:8888681649',
   },
   spanish: {
     listItem:
       'Recibí una orden judicial para ingresar a un programa de tratamiento, ¿qué debo hacer?',
     description:
-      'El tratamiento "ordenado por la corte" incluye programas para conductores bajo los efectos del alcohol o DUI, programas de desvío de drogas o PC 1000, la Proposición 36 o la Ley de Prevención del Abuso de Sustancias y Delitos (SACPA) y el Tribunal de Drogas. Aquellos a quienes el Tribunal les indique que obtengan tratamiento por cualquiera de estas violaciones o programas deben comunicarse con su oficial de libertad condicional o condicional o abogado asignado para discutir a qué programa se les puede exigir que asistan. Si tiene preguntas sobre DUI, PC1000 (Desviación de Drogas), Tribunales de Drogas o SACPA (Prop 36), puede comunicarse con la Línea de Acceso gratuita de Behavioral Wellness las 24 horas (888) 868-1649.',
+      'El tratamiento "ordenado por la corte" incluye programas de DUI o DUI, programas de desvío de drogas o PC 1000, la Proposición 36 o la Ley de Prevención del Abuso de Sustancias y Delitos (SACPA) y el Tribunal Penal. Drogas Aquellos a quienes el Tribunal les indique que obtengan tratamiento por cualquiera de estas violaciones o programas deben comunicarse con su oficial de libertad condicional o de libertad condicional o abogado asignado para discutir a qué programa se les puede exigir que asistan. Si tiene preguntas sobre DUI, PC1000 (Desviación de Drogas), Tribunales de Drogas o SACPA (Prop 36), puede comunicarse con la Línea de Acceso Gratuito las 24 Horas de Behavioral Wellness (888) 868-1649 o ',
+    href1: 'tel:8888681649',
   },
 }
 export const refill: CopyHolder = {
   english: {
     listItem: `Where can I go to have my prescription refilled?`,
-    description:
-      'To obtain an initial prescription, you must be seen by a physician. Contact the 24-hour, toll-free Access Line at (888) 868-1649 for screening and a referral.',
+    description: `To obtain an initial prescription, you must be seen by a physician.For a screening or referral contact the 24-hour, toll-free Access Line at (888) 868-1649 begin by `,
     href1: 'tel:8888681649',
   },
   spanish: {
     listItem: '¿A dónde puedo ir para que me vuelvan a surtir mi receta?',
-    description:
-      'Para obtener una receta inicial, debe ser atendido por un médico. Comuníquese con la línea de acceso gratuita las 24 horas al (888) 868-1649 para una evaluación y una remisión.',
+    description: `Para obtener una receta inicial, debe ser atendido por un médico. Para una evaluación o una derivación, comuníquese con la línea de acceso gratuita las 24 horas al (888) 868-1649 y comience `,
     href1: 'tel:8888681649',
   },
 }
@@ -108,13 +111,13 @@ export const covid: CopyHolder = {
   english: {
     listItem: `Where can I go to receive a covid vaccine?`,
     description:
-      'There are many locations to receive a covid vaccine. Visit https://publichealthsbc.org/vaccine/ for more information',
+      'There are many locations to receive a covid vaccine. Find more information by ',
     href1: 'https://publichealthsbc.org/vaccine/',
   },
   spanish: {
     listItem: '¿Dónde puedo ir para recibir una vacuna covid?',
     description:
-      'Hay muchos lugares para recibir una vacuna covid. Visite https://publichealthsbc.org/vaccine/ para obtener más información',
+      'Hay muchos lugares para recibir una vacuna covid. Encuentre más información por ',
     href1: 'https://publichealthsbc.org/vaccine/',
   },
 }
@@ -122,14 +125,14 @@ export const documents: CopyHolder = {
   english: {
     listItem: `How do I obtain my social security card, identification card, and/or birth certificate? `,
     description:
-      'You can click here and then click on the Important documents which will give you more information on how to obtain these resources',
-    href1: 'https://www.thrivesbc.com/checklist',
+      'Check out our "Important Documents" section which will give you more information on how to obtain these resources by ',
+    nextLink: '/checklist',
   },
   spanish: {
     listItem:
       '¿Cómo obtengo mi tarjeta de seguro social, tarjeta de identificación y / o certificado de nacimiento?',
     description:
-      'Puede hacer clic aquí y luego hacer clic en Documentos importantes que le brindarán más información sobre cómo obtener estos recursos',
-    href1: 'https://www.thrivesbc.com/checklist',
+      'Consulte nuestra sección "Documentos importantes", que le brindará más información sobre cómo obtener estos recursos al ',
+    nextLink: '/checklist',
   },
 }
