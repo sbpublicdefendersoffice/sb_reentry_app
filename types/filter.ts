2b1b88efe787738332719d01271606e6f82b763c
Victor <vsauceda@codeforamerica.org>
2021-07-12 20:06:12 -0700

    adding Filter Type

---
+++ types/filter.ts
@@ -0,0 +1,13 @@
+export interface Filter {
+  citySelected: string[]
+  serviceSelected: string[]
+  genderSelected: string[]
+  languageSelected: string[]
+  handleSelected: any
+
+  MenuProps: any
+  newServiceFilter: any
+  setOpen?: any
+  modalStyle?: any
+  activeCopy?: any
+}
