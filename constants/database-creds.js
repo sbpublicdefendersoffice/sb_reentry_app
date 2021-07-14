ad092e7876910b32da934b79989158aa099eecfb
timmalstead-cfa <tmalstead@codeforamerica.org>
2021-07-14 13:35:16 -0700

    changes up database credential format

--- constants/database-creds.js
+++ constants/database-creds.js
@@ -1,22 +1,21 @@
-const baseOptions = {
-  dialect: 'postgres',
-  port: 5432,
-}
+const dialect = 'postgres'
 
 const postgresEnvs = {
   development: {
-    ...baseOptions,
-    host: 'localhost',
-    database: 'thrive',
+    dialect,
     username: 'thrive',
     password: 'sbc',
+    host: 'localhost',
+    port: 5432,
+    database: 'thrive',
   },
   production: {
-    ...baseOptions,
-    host: process.env.POSTGRES_HOST,
-    database: process.env.POSTGRES_DATA,
+    dialect,
     username: process.env.POSTGRES_USER,
     password: process.env.POSTGRES_PASS,
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    database: process.env.POSTGRES_DATA,
     // ssl: true,
     // dialectOptions: {
     //   ssl: { require: true, rejectUnauthorized: false },
