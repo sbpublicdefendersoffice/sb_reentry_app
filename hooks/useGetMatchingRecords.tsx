501b73c3e351b7a54c1bd72337db9a89f5233d76
Victor <vsauceda@codeforamerica.org>
2021-07-16 10:43:16 -0700

    changing query from org_tags to locations_city and location_services

--- hooks/useGetMatchingRecords.tsx
+++ hooks/useGetMatchingRecords.tsx
@@ -1,10 +1,19 @@
-const useGetMatchingRecords = (allRecords, keywords) => {
+const getMatchingRecords = (allRecords, keywords) => {
   const filteredRecords = allRecords.records.filter(record =>
     keywords.some(keyword => {
-      let orgTags = record.fields.org_tags.map(item => item.toLowerCase())
-      return orgTags.includes(keyword.toLowerCase())
+      let grabService = record.fields.location_services?.split(',')
+      let location_services = grabService?.map(item =>
+        item.trimStart().toLowerCase(),
+      )
+      let locations_city = record.fields.locations_city?.map(item =>
+        item.toLowerCase(),
+      )
+      return (
+        location_services?.includes(keyword.toLowerCase()) ||
+        locations_city?.includes(keyword.toLowerCase())
+      )
     }),
   )
   return { records: filteredRecords, category: allRecords.category }
 }
-export default useGetMatchingRecords
+export default getMatchingRecords
