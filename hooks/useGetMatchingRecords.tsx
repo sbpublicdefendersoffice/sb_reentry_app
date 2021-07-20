165fb5a511737fa01e9bd928c59358e0af9500a0
Victor <vsauceda@codeforamerica.org>
2021-07-20 10:44:19 -0700

    fixing city filtering-

--- hooks/useGetMatchingRecords.tsx
+++ hooks/useGetMatchingRecords.tsx
@@ -1,19 +1,44 @@
+function getAllIndexes(arr, val) {
+  var indexes = [],
+    i = -1
+  while ((i = arr.indexOf(val, i + 1)) != -1) {
+    indexes.push(i)
+  }
+  return indexes
+}
 const getMatchingRecords = (allRecords, keywords) => {
-  const filteredRecords = allRecords.records.filter(record =>
+  const filteredRecords = [...allRecords.records].filter(record =>
     keywords.some(keyword => {
       let grabService = record.fields.location_services?.split(',')
       let location_services = grabService?.map(item =>
         item.trimStart().toLowerCase(),
       )
-      let locations_city = record.fields.locations_city?.map(item =>
+      let locations_city_prep = record.fields.locations_city?.map(item =>
         item.toLowerCase(),
       )
+      let location_latitude_prep = record.fields.location_latitude?.map(
+        item => item,
+      )
+      let location_longitude_prep = record.fields.location_longitude?.map(
+        item => item,
+      )
+      let indexs = getAllIndexes(locations_city_prep, keyword.toLowerCase())
+      let locations_city = indexs.map(i => locations_city_prep[i])
+      let location_latitude = indexs.map(i => location_latitude_prep[i])
+      let location_longitude = indexs.map(i => location_longitude_prep[i])
+      if (indexs.length > 0) {
+        record.fields.locations_city = locations_city
+        record.fields.location_latitude = location_latitude
+        record.fields.location_longitude = location_longitude
+      }
+
       return (
         location_services?.includes(keyword.toLowerCase()) ||
-        locations_city?.includes(keyword.toLowerCase())
+        locations_city_prep?.includes(keyword.toLowerCase())
       )
     }),
   )
+
   return { records: filteredRecords, category: allRecords.category }
 }
 export default getMatchingRecords
