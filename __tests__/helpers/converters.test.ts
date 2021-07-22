7d44e026a87a6609bba3b686c350938a7900ff62
timmalstead-cfa <tmalstead@codeforamerica.org>
2021-07-22 15:25:51 -0700

    omits now unneeded second test in __tests__/helpers/converters.test.ts

--- __tests__/helpers/converters.test.ts
+++ __tests__/helpers/converters.test.ts
@@ -1,7 +1,7 @@
 import { LocationRecord } from '../../types/records'
 import {
   dummyTranslateRecordResponse,
-  dummyTranslatedRecordWithLocation,
+  // dummyTranslatedRecordWithLocation,
 } from '../../__helpers__/dummyData'
 
 import { convertLocationsForMap } from '../../helpers/converters'
@@ -15,18 +15,18 @@ describe('converter helper function(s)', () => {
     expect(returnResponse).toEqual([])
   })
 
-  it('returns truthy when there are location records present in input', () => {
-    const returnResponse: LocationRecord[] | null = convertLocationsForMap(
-      dummyTranslatedRecordWithLocation,
-    )
+  // it('returns truthy when there are location records present in input', () => {
+  //   const returnResponse: LocationRecord[] | null = convertLocationsForMap(
+  //     dummyTranslatedRecordWithLocation,
+  //   )
 
-    const expectedReturn: string[] = [
-      '{"multiple_categories":["mentalhealth"],"single_category":"Mental Health","longitude":-13,"latitude":3,"city":"Santa Barbara","name":"Some org","uuid":"1234567890"}',
-      '{"multiple_categories":["mentalhealth"],"single_category":"Mental Health","longitude":-5,"latitude":5,"city":"Goleta","name":"Some org","uuid":"1234567890"}',
-    ]
+  //   const expectedReturn: string[] = [
+  //     '{"multiple_categories":["mentalhealth"],"single_category":"Mental Health","longitude":-13,"latitude":3,"city":"Santa Barbara","name":"Some org","uuid":"1234567890"}',
+  //     '{"multiple_categories":["mentalhealth"],"single_category":"Mental Health","longitude":-5,"latitude":5,"city":"Goleta","name":"Some org","uuid":"1234567890"}',
+  //   ]
 
-    returnResponse.forEach((record: LocationRecord, i: number) =>
-      expect(JSON.stringify(record)).toEqual(expectedReturn[i]),
-    )
-  })
+  //   returnResponse.forEach((record: LocationRecord, i: number) =>
+  //     expect(JSON.stringify(record)).toEqual(expectedReturn[i]),
+  //   )
+  // })
 })
