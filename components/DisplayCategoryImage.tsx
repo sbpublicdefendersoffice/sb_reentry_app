44621c636daee64efd1381b7cfdf04176820837d
vsauceda <vsauceda@codeforamerica.org>
2021-08-04 16:59:20 -0700

    resize image and style

--- components/DisplayCategoryImage.tsx
+++ components/DisplayCategoryImage.tsx
@@ -4,14 +4,13 @@ import { Title } from '../ui/'
 const DisplayCategoryImage = ({ displayCategory, routeCategory }) => {
   return (
     <>
-      <Grid item xs={12} style={{marginTop: "2rem"}}>
-        <Title>{displayCategory}</Title>
-      </Grid>
-      <Grid item xs={12} sm={12} md={4}>
+
+      <Grid item xs={12} sm={12} md={4} style={{maxWidth: "100%"}}>
         <img
           style={{
             width: '15rem',
-            height: '15rem',
+            height: '9rem',
+           
           }}
           src={`/icons/${routeCategory.replace(' ', '')}.svg`}
           alt={`Pic of ${displayCategory}`}
