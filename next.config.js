93d97196109ca0c4129602eea08696c4ed72bb52
timmalstead-cfa <tmalstead@codeforamerica.org>
2021-08-04 10:47:05 -0700

    cleans up next.config.js to make it work a bit better with webpack-stats-plugin

--- next.config.js
+++ next.config.js
@@ -39,18 +39,19 @@ const nextConfigOptions = {
       }
     }
 
-    config.plugins.push(
-      new StatsWriterPlugin({
-        filename: 'webpack-stats.json',
-        stats: {
-          context: './src', // optional, will improve readability of the paths
-          assets: true,
-          entrypoints: true,
-          chunks: true,
-          modules: true,
-        },
-      }),
-    )
+    process.env.NODE_ENV === 'production' &&
+      config.plugins.push(
+        new StatsWriterPlugin({
+          filename: 'webpack-stats.json',
+          stats: {
+            context: './src', // optional
+            assets: true,
+            entrypoints: true,
+            chunks: true,
+            modules: true,
+          },
+        }),
+      )
 
     return config
   },
