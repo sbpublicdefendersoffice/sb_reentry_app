a87278bde540b8781051ab30bc46d693d40a9005
vsauceda <vsauceda@codeforamerica.org>
2021-08-16 20:00:18 -0700

    adding favorite icon and logic moving on click to other elements

--- components/OrgRecordCard.tsx
+++ components/OrgRecordCard.tsx
@@ -1,10 +1,13 @@
-import { Fragment, ReactElement } from 'react'
+import { Fragment, ReactElement, useContext } from 'react'
 import { useRouter } from 'next/router'
-
+import FavoriteIcon from '@material-ui/icons/Favorite'
+import { Grid, Button } from '@material-ui/core/'
+import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder'
 import useLanguage from '../hooks/useLanguage'
 import { PGOrganizationResponse } from '../types/'
 import { ENGLISH } from '../constants/language'
 import { Card, Paragraph } from '../ui'
+import FavoriteProvider from '../hooks/useFavorite'
 export interface OrgRecordCardProps {
   record: PGOrganizationResponse
 }
@@ -21,17 +24,30 @@ const OrgRecordCard = ({ record }: OrgRecordCardProps) => {
     name_english,
     name_spanish,
   } = record
-
+  const { favoriteResources, updateFavoriteResources } =
+    useContext(FavoriteProvider)
+  const isFavIcon = <FavoriteIcon style={{ color: '#13385E', fontSize: "3rem" }} />
+  const isNotFavIcon = <FavoriteBorderIcon style={{fontSize: "3rem"}} />
+  const heart = favoriteResources.some(item => item.id === id)
+    ? isFavIcon
+    : isNotFavIcon
   const activeCategories: string[] =
     language === ENGLISH ? categories_english : categories_spanish
   const categoryTitle: string = multiple_categories
     ? multiple_categories[0].replace(' ', '')
     : 'socialservices'
   const isCategoryPage: boolean = pathname === '/[category]'
-
   const pushToRecord = (e): Promise<boolean> =>
     push(urlSlug, `/${categoryTitle}/${e.target.title}`)
-
+  const clickHeart = e => {
+    updateFavoriteResources(id, record)
+  }
+  if (localStorage.getItem('favorites') === null) {
+    localStorage.setItem('favorites', '[]')
+  }
+  if (localStorage.getItem('favoriteRecords') === null) {
+    localStorage.setItem('favoriteRecords', '[]')
+  }
   return (
     <Card
       data-testid="OrgRecordCard"
@@ -39,7 +55,6 @@ const OrgRecordCard = ({ record }: OrgRecordCardProps) => {
       title={String(id)}
       className={styles.OrgRecordCard}
       interactive
-      onClick={pushToRecord}
     >
       <img
         role="img"
@@ -49,8 +64,9 @@ const OrgRecordCard = ({ record }: OrgRecordCardProps) => {
         height="4rem"
         src={`/icons/${isCategoryPage ? query.category : categoryTitle}.svg`}
         alt={`${categoryTitle}_icon`}
+        onClick={pushToRecord}
       />
-      <div>
+      <div onClick={pushToRecord}>
         <Paragraph role="heading" title={String(id)} size="med-text">
           {name_english || name_spanish}
         </Paragraph>
@@ -67,6 +83,15 @@ const OrgRecordCard = ({ record }: OrgRecordCardProps) => {
           )}
         </Paragraph>
       </div>
+      <Grid container justify="flex-end">
+        <Button id={String(id)}
+          title={String(id)}
+          onClick={clickHeart}
+          style={{ background: 'none', border: 'none'}}
+        >
+          <div >{heart}</div>
+        </Button>
+      </Grid>
     </Card>
   )
 }
