b01db67b6e4a2c0d3221da2af776244571b9db61
timmalstead-cfa <tmalstead@codeforamerica.org>
2021-08-18 14:42:19 -0700

    cleans up jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,12 @@
 const jestConfig = {
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   testPathIgnorePatterns: [
-    '<rootDir>/.next/',
-    '<rootDir>/node_modules/',
+    '<rootDir>/(.next|node_modules)/',
     //make below work
     '<rootDir>/__tests__/components/RecordPane',
   ],
   coveragePathIgnorePatterns: [
-    '<rootDir>/__helpers__/',
-    '<rootDir>/__mocks__/',
+    '<rootDir>/(__helpers__|__mocks__/)/',
     '<rootDir>/(ui|hooks|helpers|constants|components)/index.ts',
     '<rootDir>/helpers/analytics.ts',
     '<rootDir>/next.config.js',
