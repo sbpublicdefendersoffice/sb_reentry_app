6b20e29e152410747ae9ce078226cc1271b6fe76
vsauceda <vsauceda@codeforamerica.org>
2021-08-19 22:00:37 -0700

    adding new properties for submenu links

--- types/routes.ts
+++ types/routes.ts
@@ -3,6 +3,24 @@ export interface RouteInfo {
   title_spanish?: string
   route: string
   imgPath?: string
+  itemOne_english?: string
+  itemOne_spanish?: string
+  itemOneRoute?: string
+  itemTwo_english?: string
+  itemTwo_spanish?: string
+  itemTwoRoute?: string
+  itemThree_english?: string
+  itemThree_spanish?: string
+  itemThreeRoute?: string
+  itemFour_english?: string
+  itemFour_spanish?: string
+  itemFourRoute?: string
+  itemFive_english?: string
+  itemFive_spanish?: string
+  itemFiveRoute?: string
+  itemSix_english?: string
+  itemSix_spanish?: string
+  itemSixRoute?: string
 }
 
 export interface InfoWithDescription extends RouteInfo {
