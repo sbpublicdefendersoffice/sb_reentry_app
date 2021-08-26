016c5bc136d35466a2c05bc40810f385774fe8d4
vsauceda <vsauceda@codeforamerica.org>
2021-08-25 22:30:13 -0700

    mobile search bar bug

--- components/SearchTermsMarquee.tsx
+++ components/SearchTermsMarquee.tsx
@@ -31,16 +31,21 @@ const SearchTermsMarquee = ({
 }: SearchTermsMarqueeProps) => {
   const tagsRef: MutableRefObject<HTMLParagraphElement> | null = useRef(null)
   const [isScrolling, setIsScrolling] = useState<boolean>(false)
-  const [searchTermsToScroll, setSearchTermsToScroll] = useState<
-    string[] | null
-  >(null)
+  const [searchTermsToScroll, setSearchTermsToScroll] =
+    useState<string[] | null>(null)
 
-  const setScrollingEffect = (): void =>
+  const setScrollingEffect = (): void => {
+    if (
+      !tagsRef.current?.getClientRects()[0].width ||
+      !formRef.current?.getClientRects()[0].width
+    ) {
+      return null
+    }
     tagsRef.current?.getClientRects()[0].width >
     formRef.current?.getClientRects()[0].width
       ? setIsScrolling(true)
       : setIsScrolling(false)
-
+  }
   const activeCopy = copy[language]
 
   useEffect(() => {
