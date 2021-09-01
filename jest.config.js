8d3993194fe8d3faf598669aa61029089a808297
timmalstead-cfa <tmalstead@codeforamerica.org>
2021-09-01 14:40:46 -0700

    updating next to patch security issue GHSA-9gr3-7897-pp7m . note that this requires disabling tests that use next-page-tester. this should be temporary

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,10 @@
 const jestConfig = {
+  testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   testPathIgnorePatterns: [
     '<rootDir>/(.next|node_modules)/',
+    //waiting for upgrade to next-page-tester, but needed to upgrade next for security reasons
+    '<rootDir>/__tests__/pages',
     // Below need to be fixed
     '<rootDir>/__tests__/components/RecordPane',
     '<rootDir>/__tests__/components/CityFilter',
@@ -12,6 +15,8 @@ const jestConfig = {
     '<rootDir>/components/(DisplayMap|MapMarker|PDFViewer|SearchTermsMarquee)',
     '<rootDir>/helpers/analytics.ts',
     '<rootDir>/next.config.js',
+    //waiting for upgrade to next-page-tester, but needed to upgrade next for security reasons
+    '<rootDir>/pages',
     // Below need to be fixed
     '<rootDir>/components/RecordPane',
     '<rootDir>/components/CityFilter',
