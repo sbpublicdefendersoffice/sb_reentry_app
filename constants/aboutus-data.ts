6a93c2396775bb02b54970536291ed1bfb0f07e8
‘victorSauceda’ <vsauceda@codeforamerica.org>
2021-09-07 18:39:13 -0700

    changing language to reflect all justice impacted

--- constants/aboutus-data.ts
+++ constants/aboutus-data.ts
@@ -7,7 +7,7 @@ export const aboutCopy: CopyHolder = {
     community: 'Community Outreach',
     howWeDid: 'How We did this',
     ourProcess: 'Our Process at Code for America',
-    partnered: `Code for America partnered with the Santa Barbara County Public Defender's office to develop a re-entry application for individuals who will be returning to Santa Barbara after incarceration. Jeanmarie Levy, Timothy Malstead and Victor Sauceda were assigned to developing this application in October 2020.`,
+    partnered: `Code for America partnered with the Santa Barbara County Public Defender's office to develop a resource tool for justice impacted individuals in Santa Barbara County. Jeanmarie Levy, Timothy Malstead and Victor Sauceda were assigned to developing this application in October 2020.`,
     building: `Building this application with a human-centered design approach allowed them to better understand the needs of the justice impacted. After many interviews with justice impacted individuals and community based organizations they noticed a need for a centralized location of resources. After validating over 1000 resources available across the platform to ensure accuracy they hope that you are able to benefit from the use of this resource application. Good luck in your re-entry process, Santa Barbara County is rooting for you!`,
   },
 
@@ -19,7 +19,7 @@ export const aboutCopy: CopyHolder = {
     howWeDid: 'Como lo hicimos',
     ourProcess: 'Nuestro proceso en Code for America',
     partnered:
-      'Code for America se asoció con la oficina del Defensor Público del Condado de Santa Bárbara para desarrollar una solicitud de reingreso para las personas que regresarán a Santa Bárbara después del encarcelamiento. Jeanmarie Levy, Timothy Malstead y Victor Sauceda fueron asignados a desarrollar esta aplicación en octubre de 2020.',
+      'Code for America se asoció con la oficina del Defensor Público del Condado de Santa Bárbara para desarrollar una herramienta de recursos para las personas afectadas por la justicia en el condado de Santa Bárbara. Jeanmarie Levy, Timothy Malstead y Victor Sauceda fueron asignados a desarrollar esta aplicación en octubre de 2020.',
     building: `La construcción de esta aplicación con un enfoque de diseño centrado en el ser humano les permitió comprender mejor las necesidades de la justicia afectada. Después de muchas entrevistas con personas afectadas por la justicia y organizaciones comunitarias, notaron la necesidad de una ubicación centralizada de los recursos. Después de validar más de 1000 recursos disponibles en la plataforma para garantizar la precisión, esperan que pueda beneficiarse del uso de esta aplicación de recursos. ¡Buena suerte en su proceso de reingreso, el condado de Santa Bárbara lo apoya!`,
   },
 }
