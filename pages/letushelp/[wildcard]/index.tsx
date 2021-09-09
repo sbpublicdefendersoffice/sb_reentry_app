ac51b1ff7710c01bff70ef8ae8ca8ef108ec998d
timmalstead-cfa <tmalstead@codeforamerica.org>
2021-09-09 11:07:01 -0700

    styling retouches and modifying unfinished landing pages to be bilingual

--- pages/letushelp/[wildcard]/index.tsx
+++ pages/letushelp/[wildcard]/index.tsx
@@ -1,2 +1,19 @@
-const CategoryPlaceHolder = () => <span>This category does not exist yet</span>
+import { CopyHolder } from '../../../types'
+import { useLanguage } from '../../../hooks'
+
+const copy: CopyHolder = {
+  english: {
+    category: 'This category does not exist yet',
+  },
+  spanish: {
+    category: 'Esta categoría aún no existe',
+  },
+}
+
+const CategoryPlaceHolder = () => {
+  const { language } = useLanguage()
+  const { category } = copy[language]
+
+  return <span>{category}</span>
+}
 export default CategoryPlaceHolder
