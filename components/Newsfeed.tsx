7f2acc8daff4b2159d6b14811e119d469f89c219
timmalstead-cfa <tmalstead@codeforamerica.org>
2021-10-05 12:30:10 -0700

    creates Newsfeed component

---
+++ components/Newsfeed.tsx
@@ -0,0 +1,11 @@
+import styles from './Newsfeed.module.css'
+
+const Newsfeed = () => {
+  return (
+    <section className={styles.Newsfeed}>
+      <span>newsfeed</span>
+    </section>
+  )
+}
+
+export default Newsfeed
