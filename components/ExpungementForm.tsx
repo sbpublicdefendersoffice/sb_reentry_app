18ea314b22e1bd28cdf550ffc110028d199dde88
timmalstead-cfa <tmalstead@codeforamerica.org>
2021-10-18 11:38:05 -0700

    adds expungment page and begins expungment form component

---
+++ components/ExpungementForm.tsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+
+import styles from './ExpungementForm.module.css'
+
+import { Title, Paragraph, Input, Button } from '../ui'
+
+const ExpungementForm = () => {
+  return <form className={styles.ExpungementForm}>let's expunge!</form>
+}
+
+export default ExpungementForm
---
+++ pages/expungement/index.tsx
@@ -0,0 +1,33 @@
+import { GetServerSideProps } from 'next'
+
+import { HeadTags, ExpungementForm } from '../../components'
+import { siteTitle, isDev } from '../../constants'
+
+const ExpungementPage = () => {
+  return (
+    <>
+      <HeadTags
+        title={`${siteTitle} | Record Expungement`}
+        href={`/expungement`}
+        description={`Thrive SBC Record Expungement`}
+      />
+      <ExpungementForm />
+    </>
+  )
+}
+
+export default ExpungementPage
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  if (!isDev)
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false,
+      },
+    }
+
+  return {
+    props: {},
+  }
+}
