8516abd9807408432448ccb9c747e2bedb5da909
timmalstead-cfa <tmalstead@codeforamerica.org>
2021-10-20 10:11:10 -0700

    adds marital and veteran status portion of expungement form

--- components/ExpungementMaritalAndVeteranStatus.tsx
+++ components/ExpungementMaritalAndVeteranStatus.tsx
@@ -4,18 +4,33 @@ import { CopyHolder } from '../types'
 import { Card, Paragraph, Input } from '../ui'
 import { useLanguage, useIntersectionStyle } from '../hooks'
 
-// marital status
-// marital_status_single, marital_status_married,  marital_status_separated, marital_status_divorced,marital_status_commonlaw
-
-// military experience
-// veteran_yes, veteran_no, military_branch, discharge_date,
-
 const copy: CopyHolder = {
   english: {
     status: 'Marital and Veteran Status',
-    marital: 'Marital and Veteran Status',
+    marital: 'Marital Status',
+    single: 'Single',
+    married: 'Married',
+    separated: 'Separated',
+    divorced: 'Divorced',
+    commonlaw: 'Common-law',
+    veteran: 'Are you a veteran?',
+    yes: 'Yes',
+    branch: 'If so, what branch did you serve in?',
+    discharge: 'Discharge Date',
   },
-  spanish: { status: 'Estado Civil y Veterano', marital: 'Estado Civil' },
+  spanish: {
+    status: 'Estado Civil y Veterano',
+    marital: 'Estado Civil',
+    single: 'Soltero',
+    married: 'Casado',
+    separated: 'Separado',
+    divorced: 'Divorciado',
+    commonlaw: 'Viviendo juntos',
+    veteran: '¿Eres un veterano?',
+    yes: 'Si',
+    branch: 'Si es así, ¿en qué rama sirvió?',
+    discharge: 'Fecha de alta',
+  },
 }
 
 import styles from './ExpungementForm.module.css'
@@ -34,7 +49,21 @@ const ExpungementMaritalAndVeteranStatus = ({
   const vetRef: MutableRefObject<HTMLDivElement> = useRef()
   const { language } = useLanguage()
 
-  const { status, marital } = copy[language]
+  useIntersectionStyle(vetRef, animationClass)
+
+  const {
+    status,
+    marital,
+    single,
+    married,
+    separated,
+    divorced,
+    commonlaw,
+    veteran,
+    yes,
+    branch,
+    discharge,
+  } = copy[language]
 
   return (
     <Card ref={vetRef}>
@@ -43,14 +72,69 @@ const ExpungementMaritalAndVeteranStatus = ({
       </Paragraph>
       <section>
         <label className={styles.LabelMargin}>{marital}</label>
-        {/* <label htmlFor="current_probation_yes"></label>
+        <label htmlFor="marital_status_single">{single}</label>
         <Input
           onChange={handleChange}
           type="radio"
-          name="on_probation"
-          value="current_probation_no"
-          id="current_probation_yes"
-        /> */}
+          name="marital_status"
+          value="marital_status_married;marital_status_separated;marital_status_divorced;marital_status_commonlaw"
+          id="marital_status_single"
+        />
+        <label htmlFor="marital_status_married">{married}</label>
+        <Input
+          onChange={handleChange}
+          type="radio"
+          name="marital_status"
+          value="marital_status_single;marital_status_separated;marital_status_divorced;marital_status_commonlaw"
+          id="marital_status_married"
+        />
+        <label htmlFor="marital_status_separated">{separated}</label>
+        <Input
+          onChange={handleChange}
+          type="radio"
+          name="marital_status"
+          value="marital_status_single;marital_status_married;marital_status_divorced;marital_status_commonlaw"
+          id="marital_status_separated"
+        />
+        <label htmlFor="marital_status_divorced">{divorced}</label>
+        <Input
+          onChange={handleChange}
+          type="radio"
+          name="marital_status"
+          value="marital_status_single;marital_status_married;marital_status_separated;marital_status_commonlaw"
+          id="marital_status_divorced"
+        />
+        <label htmlFor="marital_status_commonlaw">{commonlaw}</label>
+        <Input
+          onChange={handleChange}
+          type="radio"
+          name="marital_status"
+          value="marital_status_single;marital_status_married;marital_status_separated;marital_status_divorced"
+          id="marital_status_commonlaw"
+        />
+      </section>
+      <section>
+        <label className={styles.LabelMargin}>{veteran}</label>
+        <label htmlFor="veteran_yes">{yes}</label>
+        <Input
+          onChange={handleChange}
+          type="radio"
+          name="veteran_status"
+          value="veteran_no"
+          id="veteran_yes"
+        />
+        <label htmlFor="veteran_no">No</label>
+        <Input
+          onChange={handleChange}
+          type="radio"
+          name="veteran_status"
+          value="veteran_yes"
+          id="veteran_no"
+        />
+        <label htmlFor="military_branch">{branch}</label>
+        <Input onChange={handleChange} type="text" id="military_branch" />
+        <label htmlFor="discharge_date">{discharge}</label>
+        <Input onChange={handleChange} type="date" id="discharge_date" />
       </section>
     </Card>
   )
