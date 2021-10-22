57b033620fabed1b54020c1fb5e843b87185f7aa
‘victorSauceda’ <vsauceda@codeforamerica.org>
2021-10-22 14:53:05 -0700

    adding useToken / useUser hooks

---
+++ hooks/useUser.ts
@@ -0,0 +1,25 @@
+import { useState, useEffect } from 'react'
+
+import { useToken } from '../hooks'
+
+const useUser = () => {
+  const [token] = useToken()
+
+  const getPayloadFromToken = token => {
+    const encodedPayload = token.split('.')[1]
+    return JSON.parse(atob(encodedPayload))
+  }
+  const [user, setUser] = useState(() => {
+    if (!token) return null
+    return getPayloadFromToken(token)
+  })
+  useEffect(() => {
+    if (!token) {
+      setUser(null)
+    } else {
+      setUser(getPayloadFromToken(token))
+    }
+  }, [token])
+  return user
+}
+export default useUser
