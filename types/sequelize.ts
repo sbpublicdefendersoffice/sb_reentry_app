356873d74dd27a9267d8b3630fa50d9abfd46e7c
‘victorSauceda’ <vsauceda@codeforamerica.org>
2021-10-22 14:59:05 -0700

    adding props, changing from user to admin

--- types/sequelize.ts
+++ types/sequelize.ts
@@ -84,12 +84,15 @@ export interface IsThisUsefulModel extends Model {
   language: string
   comment: string
 }
-export interface UserModel extends Model {
-  // id: number
+export interface AdminModel extends Model {
+  id?: number
   created_at: string
   email: boolean
   org: string
   hashedPassword: string
+  verificationString: string
+  passwordResetCode: string
+  isVerified: boolean
 }
 export type Organization = ModelCtor<OrganizationModel>
 export type Location = ModelCtor<LocationsModel>
@@ -101,7 +104,7 @@ export type ScheduleOrganization = ModelCtor<SchedulesOrganizationsModel>
 export type ServiceLocation = ModelCtor<ServicesLocationsModel>
 export type ServiceOrganization = ModelCtor<ServicesOrganizationsModel>
 export type IsThisUseful = ModelCtor<IsThisUsefulModel>
-export type User = ModelCtor<UserModel>
+export type Admin = ModelCtor<AdminModel>
 
 export interface AllModels {
   orgObj: Organization
@@ -109,5 +112,5 @@ export interface AllModels {
   servObj: Service
   schObj: Schedule
   useObj: IsThisUseful
-  userObj: User
+  adminObj: Admin
 }
