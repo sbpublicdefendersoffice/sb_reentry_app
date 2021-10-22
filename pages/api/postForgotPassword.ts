c0f44fc0ed4941234bc23576c855601aa2fe8c44
‘victorSauceda’ <vsauceda@codeforamerica.org>
2021-10-22 14:48:05 -0700

    adding forgot password route

---
+++ pages/api/postForgotPassword.ts
@@ -0,0 +1,42 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import { v4 as uuid } from 'uuid'
+import { sendEmail } from '../../helpers'
+import initDb from '../../helpers/sequelize'
+
+const postForgotPassword = async (
+  req: NextApiRequest,
+  res: NextApiResponse,
+): Promise<void> => {
+  const { adminObj } = initDb()
+  console.log('🚨', req.body)
+  try {
+    const passwordResetCode = uuid()
+    const user = await adminObj.update(
+      { passwordResetCode: passwordResetCode },
+      { where: { email: req.body } },
+    )
+    if (user) {
+      try {
+        //@ts-ignore
+        await sendEmail({
+          to: req.body,
+          from: 'verification@thrivesbc.com',
+          subject: 'Password Reset',
+          text: `
+          To reset your password, click this link:
+          http://localhost:3000/forgotpassword/${passwordResetCode}
+          `,
+        })
+      } catch (err) {
+        const error: string = err.message
+        console.error(error)
+        res.json({ error })
+      }
+    }
+  } catch (err) {
+    console.error(err)
+    res.status(500)
+  }
+  res.status(200)
+}
+export default postForgotPassword
