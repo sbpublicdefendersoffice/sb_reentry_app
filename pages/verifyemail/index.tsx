52773de8d9d2910408cca34440284837931021b9
‘victorSauceda’ <vsauceda@codeforamerica.org>
2021-10-25 18:49:03 -0700

    adding more descriptive wording

--- pages/verifyemail/index.tsx
+++ pages/verifyemail/index.tsx
@@ -1,9 +1,15 @@
 import { useEffect } from 'react'
-
+import { useStyles } from '../../constants'
 const VerifyEmailPage = () => {
+  const classes = useStyles()
   return (
-    <div>
-      <h1>THANKS FOR SIGNING UP</h1>
+    <div className={classes.root}>
+      <h1>Thanks for signing up</h1>
+      <p className={classes.h4Style}>
+        Please verify your account by clicking on the link that was sent to the
+        email address that you provided. Make sure to look in the spam folder if
+        you do not see the email in your inbox.{' '}
+      </p>
     </div>
   )
 }
