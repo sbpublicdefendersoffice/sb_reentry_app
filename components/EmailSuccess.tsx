c7060fe2e55506417c1a963fba4d350a99fa2882
‘victorSauceda’ <vsauceda@codeforamerica.org>
2021-10-25 18:34:59 -0700

    adding more descriptive wording, styling

--- components/EmailSuccess.tsx
+++ components/EmailSuccess.tsx
@@ -1,15 +1,31 @@
 import { useRouter } from 'next/router'
-
+import { useStyles } from '../constants'
+import { Button } from '@mui/material/'
 const EmailSuccess = () => {
   const { push } = useRouter()
-
+  const classes = useStyles()
   return (
-    <div className="content-container">
-      <h1>Success!</h1>
-      <p>
-        Thanks for verifying your email, now you can use all the app's features.
+    <div className={classes.root}>
+      <h1 className={classes.h4Style}>
+        Your email verification was successful
+      </h1>
+      <p className={classes.centerFlex}>
+        You will be notified from our staff once your account is ready. Please
+        allow 2-3 business days for our staff to reach out to you via the email
+        address you provided. Once approved as an organization, you will have
+        full access to update your organizations information on our dashboard.
       </p>
-      <button onClick={() => push('/dashboard')}>Go to home page</button>
+      <Button
+        size="large"
+        style={{
+          backgroundColor: '#04A868',
+          color: 'white',
+        }}
+        className={classes.downloadButtons}
+        onClick={() => push('/')}
+      >
+        <h3 style={{ padding: '1rem' }}> Back to home page</h3>
+      </Button>
     </div>
   )
 }
