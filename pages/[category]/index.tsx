59e5ae2becf76e2bf2e1d476a9cf2457df1257ad
victorSauceda <victorasauceda@gmail.com>
2021-10-29 17:52:13 -0700

    removing withwidth

--- pages/[category]/index.tsx
+++ pages/[category]/index.tsx
@@ -4,7 +4,6 @@ import { useRouter } from 'next/router'
 import { Op } from 'sequelize'
 import { useState } from 'react'
 import { Modal, Hidden, Grid } from '@mui/material'
-// import { WithWidth } from '@mui/material/'
 
 import {
   useConvertedLocationRecords,
