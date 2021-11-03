7a3532d0244618389d16505d868a038d3591ef0c
victorSauceda <victorasauceda@gmail.com>
2021-11-03 14:55:00 -0700

    changing name of file to postCBO to be more specific, removing cookie call

--- pages/api/postVerifyEmail.ts
+++ pages/api/postVerifyEmail.ts
@@ -1,41 +1,26 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 import initDb from '../../helpers/sequelize'
 
-import jwt from 'jsonwebtoken'
-
 const postVerifyEmail = async (
   req: NextApiRequest,
   res: NextApiResponse,
 ): Promise<void> => {
-  const { adminObj } = initDb()
+  const { cboObj } = initDb()
   const verificationString = JSON.parse(req.body)
 
-  const user = await adminObj.update(
+  const cbo = await cboObj.update(
     { isVerified: true },
-    { where: { verificationString } },
+    { where: { verificationString: verificationString } },
   )
 
-  const userIsVerified: boolean = Boolean(user[0])
+  const cboIsVerified: boolean = Boolean(cbo[0])
 
   try {
-    if (!userIsVerified) {
+    if (!cboIsVerified) {
       return res
         .status(401)
         .json({ message: 'The email verification code is incorrect' })
     }
-    //@ts-ignore
-    const { id, email } = user
-
-    // aside from not being the best place to set the login cookie, the below will not work because user is NOT returning id and email. sice you have only requested to change isVerified, it is only returning an array https://sequelize.org/master/manual/model-querying-basics.html#simple-update-queries
-    jwt.sign(
-      { id, email, isVerified: true },
-      process.env.JWT_SECRET,
-      { expiresIn: '2d' },
-      (err, token) => {
-        if (err) return res.status(500)
-        res.status(200).json({ token })
-      },
-    )
   } catch (err) {
     const error: string = err.message
     console.error(error)
