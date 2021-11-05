51dd4d7736994f63eee9bb5baded13aa6bdccc49
timmalstead-cfa <tmalstead@codeforamerica.org>
2021-11-05 16:35:20 -0700

    begins to refine form with input from amanda paison

--- components/ExpungementForm.tsx
+++ components/ExpungementForm.tsx
@@ -10,17 +10,7 @@ import { useRouter } from 'next/router'
 import { useLanguage, useIntersectionStyle, useToast } from '../hooks'
 import { validations, states } from '../constants'
 import { ExpungementInfo, CopyHolder, Validation } from '../types'
-import {
-  //   ExpungementMainInfo,
-  //   ExpungementProbationInfo,
-  //   ExpungementMaritalAndVeteranStatus,
-  //   ExpungementDependents,
-  //   ExpungementCaseInfo,
-  //   ExpungementEmploymentAndIncome,
-  //   ExpungementMonthlyExpenses,
-  //   ExpungementOtherIncomeAssets,
-  ExpungementSignature,
-} from './'
+import { ExpungementSignature } from './'
 
 import styles from './ExpungementForm.module.css'
 
@@ -30,11 +20,9 @@ const copy: CopyHolder = {
   english: {
     title: 'Apply for Criminal Record Expungement',
     elgible: 'You are not eligible for this relief if',
-    one: 'You are serving a sentence, are on probation, or are charged with a crime',
-    two: 'You have not complied with the terms and conditions of probation',
-    three:
-      'If it has been less than a year since your date of conviction and you were not granted probation',
-    four: 'You went to prison for this offense',
+    one: 'You are currently involved in an active prosecution',
+    two: 'You are currently serving a sentence in jail or prison',
+    three: 'You are currently on probation',
     submit: 'Submit Information',
     uptrust: 'Uptrust Enrollment',
     enroll:
@@ -88,20 +76,41 @@ const copy: CopyHolder = {
     income_source: 'Where does your income come from?',
     savings: 'How much money do you have saved?',
     unemployment_benefits: 'Do you collect unemployment benefits?',
-    expenses:
-      'About how much do you spend each month on things like rent, groceries, utilities, medical expenses, or childcare expenses?',
+    expenses: 'About how much do you spend on your monthly expenses?',
     total: 'Total expenses',
     disclaimer:
       "Please ensure that all of the information you have filled out is correct before submitting, once submitted you will not be able to alter it without contacting the Public Defender's office directly",
+    maritalExplain:
+      'Your marital status may affect your eligibility for expungement',
+    prop64:
+      "California's Propostion 64 allows many crimes involving marijuana to be expunged",
+    incomeExplain:
+      'Income can include money earned from a job, unemployment benefits, social security or retirement income, food stamps or any way that you receive money on a regular schedule',
+    exact:
+      'If you do not have an exact figure, please estimate to the nearest hundred',
+    saveExplain:
+      'The amount of money you have saved may affect your eligibility for expungement',
+    multiple: 'Please select all that apply',
+    expenseExplain:
+      'Expenses can include anything you regularly spend money on; such as rent, mortgage, groceries, utilities, medical expenses, or childcare',
+    heading:
+      "The Public Defender's Office helps those who need legal support in their cases",
+    heading2:
+      'The information you provide in this form will help us understand how we can help you',
+    heading3: 'All information will be kept confidential',
+    heading4:
+      'If there is no answer to any of the below questions, please mark “0” or “N/A”',
+    realEstate: 'Do you own a home or have a rental property?',
+    realEstateExplain:
+      'The amount of equity in your property may affect your eligibility for expungement',
+    value: 'Value',
   },
   spanish: {
     title: 'Solicite la eliminación de antecedentes penales',
     elgible: 'No es elegible para este alivio si',
-    one: 'Está cumpliendo una sentencia, está en libertad condicional o está acusado de un delito',
-    two: 'No ha cumplido con los términos y condiciones de la libertad condicional',
-    three:
-      'Si ha pasado menos de un año desde la fecha de su condena y no se le concedió libertad condicional',
-    four: 'Fuiste a prisión por este delito',
+    one: 'Actualmente está involucrado en un enjuiciamiento activo',
+    two: 'Actualmente está cumpliendo una sentencia en la cárcel o prisión',
+    three: 'Actualmente estás en libertad condicional (probation)',
     submit: 'Enviar información',
     uptrust: 'Inscripción Uptrust',
     enroll:
@@ -157,11 +166,34 @@ const copy: CopyHolder = {
     income_source: '¿De dónde provienen sus ingresos?',
     savings: '¿Cuánto dinero has ahorrado?',
     unemployment_benefits: '¿Cobran prestaciones por desempleo?',
-    expenses:
-      '¿Aproximadamente cuánto gasta cada mes en cosas como alquiler, comestibles, servicios públicos, gastos médicos o gastos de cuidado de niños?',
+    expenses: '¿Aproximadamente cuánto gasta en sus gastos mensuales?',
     total: 'Gastos totales',
     disclaimer:
       'Asegúrese de que toda la información que haya completado sea correcta antes de enviarla; una vez enviada, no podrá modificarla sin comunicarse directamente con la oficina del Defensor Público.',
+    maritalExplain:
+      'Su estado civil puede afectar su elegibilidad para eliminación de antecedentes penales',
+    prop64:
+      'La Propuesta 64 de California permite eliminar muchos delitos relacionados con la marihuana',
+    incomeExplain:
+      'Los ingresos pueden incluir dinero ganado en un trabajo, beneficios de desempleo, seguridad social o ingresos de jubilación, cupones de alimentos o cualquier forma en que reciba dinero en un horario regular',
+    exact:
+      'Si no tiene una cifra exacta, por favor calcule a la centena más cercana',
+    saveExplain:
+      'La cantidad de dinero que ha ahorrado puede afectar su elegibilidad para la eliminación de antecedentes penales.',
+    multiple: 'Por favor seleccione todas las respuestas válidas',
+    expenseExplain:
+      'Los gastos pueden incluir todo aquello en lo que gasta dinero habitualmente; como alquiler, hipoteca, comestibles, servicios públicos, gastos médicos o cuidado de niños',
+    heading:
+      'La Defensoría Pública ayuda a quienes necesitan apoyo legal en sus casos',
+    heading2:
+      'La información que proporcione en este formulario nos ayudará a comprender cómo podemos ayudarlo',
+    heading3: 'Toda la información se mantendrá confidencial',
+    heading4:
+      'Si no hay respuesta a alguna de las siguientes preguntas, marque "0" o "N/A"',
+    realEstate: '¿Es dueño de una casa o tiene una propiedad de alquiler?',
+    realEstateExplain:
+      'La cantidad de equidad en su propiedad puede afectar su elegibilidad para la eliminación de antecedentes penales',
+    value: 'Valor',
   },
 }
 
@@ -181,7 +213,6 @@ const ExpungementForm = () => {
     one,
     two,
     three,
-    four,
     success,
     name,
     alias,
@@ -226,12 +257,26 @@ const ExpungementForm = () => {
     frequency,
     week,
     month,
-    income_source,
+    // income_source,
     savings,
-    unemployment_benefits,
+    // unemployment_benefits,
     expenses,
     total,
     disclaimer,
+    maritalExplain,
+    prop64,
+    incomeExplain,
+    exact,
+    saveExplain,
+    multiple,
+    expenseExplain,
+    heading,
+    heading2,
+    heading3,
+    heading4,
+    realEstate,
+    realEstateExplain,
+    value,
   } = copy[language]
 
   const [expungeInfo, setExpungeInfo] = useState<ExpungementInfo | null>(null)
@@ -243,12 +288,12 @@ const ExpungementForm = () => {
   ): Promise<void> => {
     e.preventDefault()
     try {
+      let tempInfo: ExpungementInfo = expungeInfo
+
       validations.forEach((v: Validation): void => {
         const { error, field, id } = v
-        if (!expungeInfo[field]) throw new Error(`${error[language]}&&#${id}`)
+        if (!tempInfo[field]) throw new Error(`${error[language]}&&#${id}`)
       })
-
-      let tempInfo: ExpungementInfo = expungeInfo
 
       if (tempInfo?.['Textfield-13'] !== total)
         tempInfo = { ...tempInfo, 'Textfield-13': total }
@@ -298,13 +343,20 @@ const ExpungementForm = () => {
   return (
     <form className={styles.ExpungementForm} onSubmit={submitExpungementForm}>
       <Title>{title}</Title>
+      <Paragraph style={{ marginTop: 'var(--margin-lg)' }} size="med-text">
+        {heading}
+      </Paragraph>
+      <Paragraph size="med-text">{heading2}</Paragraph>
+      <Paragraph size="med-text">{heading3}</Paragraph>
+      <Paragraph style={{ marginBottom: 'var(--margin-lg)' }} size="med-text">
+        {heading4}
+      </Paragraph>
       <Paragraph color="highlight" size="heading-text">
         {elgible}
       </Paragraph>
       <Paragraph size="med-text">1) {one}</Paragraph>
       <Paragraph size="med-text">2) {two}</Paragraph>
       <Paragraph size="med-text">3) {three}</Paragraph>
-      <Paragraph size="med-text">4) {four}</Paragraph>
       <Card ref={formRef} className={styles.Card}>
         <section className={Field}>
           <label id="name-label" htmlFor="Full Name">
@@ -424,6 +476,7 @@ const ExpungementForm = () => {
         </section>
         <section className={Field}>
           <label>{communicate}</label>
+          <Paragraph color="deselected">{multiple}</Paragraph>
           <Card className={RadioCard}>
             <label htmlFor="Email">E-mail</label>
             <Input onChange={handleChange} type="checkbox" id="Email" />
@@ -432,6 +485,39 @@ const ExpungementForm = () => {
             <label htmlFor="Text">{text}</label>
             <Input onChange={handleChange} type="checkbox" id="Text" />
           </Card>
+        </section>
+        <section className={Field}>
+          <label>{currently_on_probation}</label>
+          <Card className={RadioCard}>
+            <label htmlFor="current_probation_yes">{yes}</label>
+            <Input
+              onChange={handleChange}
+              type="radio"
+              name="Are you currently on probation or parole"
+              value="Are you currently on probation or parole_yes_On"
+              id="current_probation_yes"
+            />
+            <label htmlFor="current_probation_no">No</label>
+            <Input
+              onChange={handleChange}
+              type="radio"
+              name="Are you currently on probation or parole"
+              value="Are you currently on probation or parole_no_On"
+              id="current_probation_no"
+            />
+            <label htmlFor="current_probation_unsure">{dont_know}</label>
+            <Input
+              onChange={handleChange}
+              type="radio"
+              name="Are you currently on probation or parole"
+              value="Are you currently on probation or parole_unsure If yes where_On"
+              id="current_probation_unsure"
+            />
+          </Card>
+        </section>
+        <section className={Field}>
+          <label htmlFor="unsure If yes where">{where}</label>
+          <Input onChange={handleChange} type="text" id="unsure If yes where" />
         </section>
         <section className={Field}>
           <label>{purpose}</label>
@@ -467,6 +553,7 @@ const ExpungementForm = () => {
         </section>
         <section className={Field}>
           <label>{marital}</label>
+          <Paragraph color="deselected">{maritalExplain}</Paragraph>
           <Card className={RadioCard}>
             <label htmlFor="marital_status_single">{single}</label>
             <Input
@@ -523,6 +610,7 @@ const ExpungementForm = () => {
         </section>
         <section className={Field}>
           <label>{marijuana}</label>
+          <Paragraph color="deselected">{prop64}</Paragraph>
           <Card className={RadioCard}>
             <label htmlFor="marijuana_related_yes">{yes}</label>
             <Input
@@ -572,39 +660,6 @@ const ExpungementForm = () => {
           </Card>
         </section>
         <section className={Field}>
-          <label>{currently_on_probation}</label>
-          <Card className={RadioCard}>
-            <label htmlFor="current_probation_yes">{yes}</label>
-            <Input
-              onChange={handleChange}
-              type="radio"
-              name="Are you currently on probation or parole"
-              value="Are you currently on probation or parole_yes_On"
-              id="current_probation_yes"
-            />
-            <label htmlFor="current_probation_no">No</label>
-            <Input
-              onChange={handleChange}
-              type="radio"
-              name="Are you currently on probation or parole"
-              value="Are you currently on probation or parole_no_On"
-              id="current_probation_no"
-            />
-            <label htmlFor="current_probation_unsure">{dont_know}</label>
-            <Input
-              onChange={handleChange}
-              type="radio"
-              name="Are you currently on probation or parole"
-              value="Are you currently on probation or parole_unsure If yes where_On"
-              id="current_probation_unsure"
-            />
-          </Card>
-        </section>
-        <section className={Field}>
-          <label htmlFor="unsure If yes where">{where}</label>
-          <Input onChange={handleChange} type="text" id="unsure If yes where" />
-        </section>
-        <section className={Field}>
           <label htmlFor="Number of Dependents">{dependents}</label>
           <Input
             onChange={handleChange}
@@ -614,6 +669,8 @@ const ExpungementForm = () => {
         </section>
         <section className={Field}>
           <label htmlFor="Take Home Pay">{income}</label>
+          <Paragraph color="deselected">{incomeExplain}</Paragraph>
+          <Paragraph color="deselected">{exact}</Paragraph>
           <Input onChange={handleChange} type="number" id="Take Home Pay" />
         </section>
         <section className={Field}>
@@ -629,15 +686,17 @@ const ExpungementForm = () => {
             <Input onChange={handleChange} type="checkbox" id="Monthly" />
           </Card>
         </section>
-        <section className={Field}>
+        {/* <section className={Field}>
           <label htmlFor="Employer Name">{income_source}</label>
           <Input onChange={handleChange} type="text" id="Employer Name" />
-        </section>
+        </section> */}
         <section className={Field}>
           <label htmlFor="Textfield-12">{savings}</label>
+          <Paragraph color="deselected">{saveExplain}</Paragraph>
+          <Paragraph color="deselected">{exact}</Paragraph>
           <Input onChange={handleChange} type="number" id="Textfield-12" />
         </section>
-        <section className={Field}>
+        {/* <section className={Field}>
           <label>{unemployment_benefits}</label>
           <Card className={RadioCard}>
             <label htmlFor="unemployed_benefits_yes">{yes}</label>
@@ -657,88 +716,37 @@ const ExpungementForm = () => {
               id="unemployed_benefits_no"
             />
           </Card>
+        </section> */}
+        <section className={Field}>
+          <label>{realEstate}</label>
+          <Paragraph color="deselected">{realEstateExplain}</Paragraph>
+          <Paragraph color="deselected">{exact}</Paragraph>
+          <Card className={RadioCard}>
+            <label htmlFor="real_estate_yes">{yes}</label>
+            <Input
+              onChange={handleChange}
+              type="radio"
+              id="real_estate_yes"
+              name="Real Estate"
+              value="Real Estate_Yes_On"
+            />
+            <label htmlFor="real_estate_no">No</label>
+            <Input type="radio" id="real_estate_no" name="Real Estate" />
+            {expungeInfo?.['Real Estate'] === 'Real Estate_Yes_On' && (
+              <>
+                <label htmlFor="Textfield-9">{value}</label>
+                <Input onChange={handleChange} type="number" id="Textfield-9" />
+              </>
+            )}
+          </Card>
         </section>
         <section className={Field}>
           <label htmlFor="Textfield-14">{expenses}</label>
+          <Paragraph color="deselected">{expenseExplain}</Paragraph>
+          <Paragraph color="deselected">{exact}</Paragraph>
           <Input onChange={handleChange} type="number" id="Textfield-14" />
         </section>
       </Card>
-      {/* <ExpungementMainInfo
-        otherLang={expungeInfo?.Other}
-        handleChange={handleChange}
-        animationClass={Load}
-      /> */}
-      {/* <Card ref={uptrustRef}>
-        <Paragraph size="med-text" color="highlight">
-          {uptrust}
-        </Paragraph>
-        <div style={{ textAlign: 'center' }}>
-          <label
-            htmlFor="I would like to be enrolled in Uptrust to receive"
-            className={styles.LabelMargin}
-          >
-            {enroll}
-          </label>
-          <Input
-            type="checkbox"
-            id="I would like to be enrolled in Uptrust to receive"
-            onChange={handleChange}
-          />
-        </div>
-      </Card> */}
-      {/* <ExpungementMaritalAndVeteranStatus
-        applicantIsVeteran={
-          expungeInfo?.['Are you a veteran'] === 'Are you a veteran_Yes_On'
-        }
-        handleChange={handleChange}
-        animationClass={Load}
-      /> */}
-      {/* <ExpungementCaseInfo
-        convictedInCounty={
-          expungeInfo?.['Convicted in Santa Barbara County'] ===
-          'Convicted in Santa Barbara County_yes_On'
-        }
-        handleChange={handleChange}
-        animationClass={Load}
-      /> */}
-      {/* <ExpungementProbationInfo
-        probationOrParole={
-          expungeInfo?.['Are you currently on probation or parole'] ===
-          'Are you currently on probation or parole_yes_On'
-        }
-        grantedProbation={
-          expungeInfo?.['Granted probation'] === 'Granted probation_yes_On'
-        }
-        handleChange={handleChange}
-        animationClass={Load}
-      /> */}
-      {/* <ExpungementDependents
-        hasDependents={!!Number(expungeInfo?.['Number of Dependents'])}
-        handleChange={handleChange}
-        animationClass={Load}
-      /> */}
-      {/* <ExpungementEmploymentAndIncome
-        unemployment={
-          expungeInfo?.['Unemployment Benefits'] ===
-          'Unemployment Benefits_Yes_On'
-        }
-        partnerUnemployment={
-          expungeInfo?.['Unemployment Benefits-0'] ===
-          'Unemployment Benefits_Yes-0_On'
-        }
-        handleChange={handleChange}
-        animationClass={Load}
-      /> */}
-      {/* <ExpungementMonthlyExpenses
-        hasOtherExpenses={!!expungeInfo?.['Textfield-13']}
-        handleChange={handleChange}
-        animationClass={Load}
-      /> */}
-      {/* <ExpungementOtherIncomeAssets
-        hasRealEstate={expungeInfo?.['Real Estate'] === 'Real Estate_Yes_On'}
-        handleChange={handleChange}
-        animationClass={Load}
-      /> */}
       <ExpungementSignature
         setExpungeInfo={setExpungeInfo}
         expungeInfo={expungeInfo}
