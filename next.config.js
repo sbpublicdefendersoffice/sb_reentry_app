62fd9c3a1c2d454ae1ff0c364b1aab486d9aa98c
timmalstead-cfa <tmalstead@codeforamerica.org>
2021-11-11 10:02:38 -0800

    fixes, hopefully, docker deployment errors by commenting out circular dependency plugin in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,4 @@
-const CircularDependencyPlugin = require('circular-dependency-plugin')
+// const CircularDependencyPlugin = require('circular-dependency-plugin')
 
 // watch out with adding external sources and apis in production mode, make sure to add them to CSP below
 const nextConfigOptions = {
@@ -92,18 +92,18 @@ const nextConfigOptions = {
     )
       config.devtool = 'source-map'
 
-    config.plugins.push(
-      new CircularDependencyPlugin({
-        // exclude detection of files based on a RegExp
-        exclude: /node_modules/,
-        // add errors to webpack instead of warnings
-        failOnError: true,
-        // allow import cycles that include an asyncronous import,
-        // e.g. via import(/* webpackMode: "weak" */ './file.js')
-        allowAsyncCycles: false,
-        cwd: process.cwd(),
-      }),
-    )
+    // config.plugins.push(
+    //   new CircularDependencyPlugin({
+    //     // exclude detection of files based on a RegExp
+    //     exclude: /node_modules/,
+    //     // add errors to webpack instead of warnings
+    //     failOnError: true,
+    //     // allow import cycles that include an asyncronous import,
+    //     // e.g. via import(/* webpackMode: "weak" */ './file.js')
+    //     allowAsyncCycles: false,
+    //     cwd: process.cwd(),
+    //   }),
+    // )
 
     return config
   },
