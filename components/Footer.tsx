5b51d648d6adaafcf687a80a9003994f4a1af87b
victorSauceda <victorasauceda@gmail.com>
2021-11-17 19:49:05 -0800

    adding social media sites/icons

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -12,6 +12,10 @@ import {
   ResourcesSupportRoutes,
 } from '../constants'
 import styles from './Footer.module.css'
+import { useStyles } from '../constants/materialStyles'
+import TwitterIcon from '@mui/icons-material/Twitter'
+import FacebookIcon from '@mui/icons-material/Facebook'
+import InstagramIcon from '@mui/icons-material/Instagram'
 const lastStaticRouteIndex: number = staticPageRoutes.length - 1
 const currentYear: number = new Date().getFullYear()
 export const copyright: string = `© Copyright 2021${
@@ -20,6 +24,9 @@ export const copyright: string = `© Copyright 2021${
 export const linkInfo = {
   text: "Santa Barbara County Public Defender's Office",
   href: 'https://www.countyofsb.org/defender',
+  twitter: 'https://twitter.com/thrive_sbc',
+  instagram: 'https://www.instagram.com/thrivesbc',
+  facebook: 'https://www.facebook.com/ThriveSantaBarbaraCounty',
 }
 const copy: CopyHolder = {
   english: {
@@ -38,6 +45,7 @@ const copy: CopyHolder = {
 }
 const Footer = () => {
   const { language } = useLanguage()
+  const classes = useStyles()
   const activeCopy = copy[language]
   const StaticPages: ReactElement[] = staticPageRoutes.map(
     (routeData: RouteInfo, i: number) => {
@@ -243,6 +251,35 @@ const Footer = () => {
       <Grid container className={styles.Footer}>
         <Grid item xs={12} sm={12} md={8}>
           {StaticPages}
+          <div id="12" className={classes.socialMediaIcons}>
+            <h3 className={classes.socialMediaIconsHeader}>Follow us</h3>
+            <div>
+              <a
+                href={linkInfo.twitter}
+                target="_blank"
+                rel="noopener noreferrer"
+                style={{ padding: '1rem', fontSize: '2rem' }}
+              >
+                <TwitterIcon style={{ fontSize: '2rem' }} />
+              </a>
+              <a
+                href={linkInfo.instagram}
+                target="_blank"
+                rel="noopener noreferrer"
+                style={{ padding: '1rem', fontSize: '2rem !important' }}
+              >
+                <InstagramIcon style={{ fontSize: '2rem' }} />
+              </a>
+              <a
+                href={linkInfo.facebook}
+                style={{ padding: '1rem', fontSize: '2rem' }}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FacebookIcon style={{ fontSize: '2rem' }} />
+              </a>
+            </div>
+          </div>
         </Grid>
         <Grid item xs={12} sm={12} md={4} className={styles.GridRight}>
           <div>
