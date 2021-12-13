fda13ba5712683eaea58a479a1ab5613001de9d7
timmalstead-cfa <tmalstead@codeforamerica.org>
2021-12-13 14:55:20 -0800

    starts apply tag

--- pages/freshstart/index.tsx
+++ pages/freshstart/index.tsx
@@ -1,41 +1,15 @@
-import { GetServerSideProps, GetServerSidePropsContext } from 'next'
-import { ReactNode, CSSProperties } from 'react'
-import { useRouter } from 'next/router'
+import type { GetServerSideProps, GetServerSidePropsContext } from 'next'
 import { JwtPayload, verify } from 'jsonwebtoken'
 
-import { HeadTags } from '../../components'
-import { CallToAction, Title, Button } from '../../ui'
+import { HeadTags, FreshStartApplyTag } from '../../components'
+import { AdaptiveFlexContainer } from '../../ui'
 import { siteTitle } from '../../constants'
-import { CopyHolder } from '../../types'
 import { useLanguage } from '../../hooks'
 
-interface FreshStartLandingPageProps {
+export interface FreshStartLandingPageProps {
   isLoggedIn: boolean
   hasAppliedForExpungement: boolean
   isVerified: boolean
-}
-
-const copy: CopyHolder = {
-  english: {
-    notLoggedIn: 'You are not logged in',
-    notVerified: 'You have not verified your email address with ThriveSBC',
-    verifiedNotApplied:
-      'You have been verified but have not applied for expungement',
-    applied: 'You have applied for expungement',
-  },
-  spanish: {
-    notLoggedIn: 'Usted no se ha identificado',
-    notVerified:
-      'No ha verificado su dirección de correo electrónico con ThriveSBC',
-    verifiedNotApplied:
-      'Ha sido verificado, pero no ha solicitado la eliminación.',
-    applied: 'Ha solicitado la eliminación de antecedentes penales',
-  },
-}
-
-const buttonStyle: CSSProperties = {
-  height: 'min-content',
-  marginTop: 'var(--pad-std)',
 }
 
 const FreshStartLandingPage = ({
@@ -43,27 +17,7 @@ const FreshStartLandingPage = ({
   hasAppliedForExpungement,
   isVerified,
 }: FreshStartLandingPageProps) => {
-  const { push } = useRouter()
   const { language } = useLanguage()
-
-  const { notLoggedIn, notVerified, verifiedNotApplied, applied } =
-    copy[language]
-
-  const logOut = async (): Promise<void> => {
-    const loggingOut: Response = await fetch('/api/logout')
-    const logoutMessage = await loggingOut.json()
-    if (logoutMessage.error) console.log('oh no!')
-    else {
-      console.log('oh yeah')
-      push('/login')
-    }
-  }
-
-  const logOutButton: ReactNode = (
-    <Button style={buttonStyle} onClick={logOut}>
-      Logout
-    </Button>
-  )
 
   return (
     <>
@@ -72,32 +26,13 @@ const FreshStartLandingPage = ({
         href={`/freshstart`}
         description={`Thrive SBC | Fresh Start Information`}
       />
-      <CallToAction>
-        {!isLoggedIn ? (
-          <Title>{notLoggedIn}</Title>
-        ) : !isVerified ? (
-          <>
-            <Title>{notVerified}</Title>
-            {logOutButton}
-          </>
-        ) : isVerified && hasAppliedForExpungement ? (
-          <>
-            <Title>{applied}</Title>
-            {logOutButton}
-          </>
-        ) : (
-          <>
-            <Title>{verifiedNotApplied}</Title>
-            <Button
-              style={buttonStyle}
-              onClick={() => push('/freshstart/apply')}
-            >
-              Apply for Expungement
-            </Button>
-            {logOutButton}
-          </>
-        )}
-      </CallToAction>
+      <AdaptiveFlexContainer>
+        <FreshStartApplyTag
+          isLoggedIn={isLoggedIn}
+          hasAppliedForExpungement={hasAppliedForExpungement}
+          isVerified={isVerified}
+        />
+      </AdaptiveFlexContainer>
     </>
   )
 }
