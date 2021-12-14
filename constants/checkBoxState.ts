51ad765a03a4e3082c4527cb127bd7168db2d864
victorSauceda <victorasauceda@gmail.com>
2021-12-13 20:02:37 -0800

    adding category initial state to constants

---
+++ constants/checkBoxState.ts
@@ -0,0 +1,58 @@
+export const initCategoriesState = [
+  {
+    categories_english: 'Community Support',
+    categories_spanish: 'Apoyo Comunitario',
+    checked: false,
+  },
+  {
+    categories_english: 'Substance Use',
+    categories_spanish: 'Uso de Sustancias',
+    checked: false,
+  },
+  {
+    categories_english: 'Transportation',
+    categories_spanish: 'Transporte',
+    checked: false,
+  },
+  { categories_english: 'Food', categories_spanish: 'Comida', checked: false },
+  {
+    categories_english: 'Housing',
+    categories_spanish: 'Alojamiento',
+    checked: false,
+  },
+  {
+    categories_english: 'Mental Health',
+    categories_spanish: 'Salud Mental',
+    checked: false,
+  },
+  {
+    categories_english: 'Resource Directory',
+    categories_spanish: 'Directorio de Recursos',
+    checked: false,
+  },
+  {
+    categories_english: 'Medical Support',
+    categories_spanish: 'Apoyo Médico',
+    checked: false,
+  },
+  {
+    categories_english: 'Legal Services',
+    categories_spanish: 'Servicios Jurídicos',
+    checked: false,
+  },
+  {
+    categories_english: 'Employment',
+    categories_spanish: 'Empleo',
+    checked: false,
+  },
+  {
+    categories_english: 'Clothing',
+    categories_spanish: 'Ropa',
+    checked: false,
+  },
+  {
+    categories_english: 'Social Services',
+    categories_spanish: 'Servicios Sociales',
+    checked: false,
+  },
+]
