8e4251e343db4c32cec89ed5e38eb5f4a5a63839
victorSauceda <victorasauceda@gmail.com>
2021-12-23 19:19:41 -0800

    reformatting helpers

--- helpers/timeConverter.ts
+++ helpers/timeConverter.ts
@@ -1,27 +1,19 @@
 const timeConverter = time => {
-  if (time.length == 4) {
-    let militaryTime = time
-    militaryTime.split(':')
-    let hours = Number(militaryTime[0])
-    let minutes = Number(militaryTime[0])
-
-    time
-    let timeValue
-    if (hours > 0 && hours <= 12) {
-      timeValue = '' + hours
-    } else if (hours > 12) {
-      timeValue = '' + (hours - 12)
-    } else if (hours == 0) {
-      timeValue = '12'
-    }
-    timeValue += minutes < 10 ? ':0' + minutes : ':' + minutes // get minutes
-    timeValue += ':00'
-    timeValue += hours >= 12 ? ' PM' : ' AM' // get AM/PM
-    console.log(timeValue, 'in time value')
-    return new Date(
-      `Mon Aug 18 2014 ${timeValue} GMT-0700 (Pacific Daylight Time)`,
-    )
+  let militaryTime = time
+  let splitTime = militaryTime.split(':')
+  let hours = splitTime[0]
+  let minutes = splitTime[1]
+  let timeValue
+  if (hours > 0 && hours <= 12) {
+    timeValue = '' + hours
+  } else if (hours > 12) {
+    timeValue = '' + (hours - 12)
+  } else if (hours == 0) {
+    timeValue = '12'
   }
-  return
+  timeValue += minutes < 10 ? ':0' + minutes : ':' + minutes // get minutes
+  timeValue += ':00'
+  timeValue += hours >= 12 ? ' PM' : ' AM' // get AM/PM
+  return `Mon Aug 18 2014 ${timeValue} GMT-0700 (Pacific Daylight Time)`
 }
 export default timeConverter
