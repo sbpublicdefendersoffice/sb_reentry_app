9abde72c21b0e938893972c5c1bedfe5d811a17d
Bryan Burzon <bburzon@countyofsb.org>
2022-03-02 15:11:22 -0800

    wip: added new fields for CBO sign up screen

--- pages/api/postSignup.ts
+++ pages/api/postSignup.ts
@@ -15,7 +15,7 @@ const postSignup = async (
 ): Promise<void> => {
   try {
     const body = JSON.parse(req.body)
-    let { email, pwd, org, signupType } = body
+    let { email, pwd, org, orgWebsite, signupType, customers } = body
 
     if (email && pwd) {
       if (signupType && signupType === 'client') {
@@ -98,6 +98,8 @@ const postSignup = async (
 
         ✅ Make sure the account has been verified in the database. 
         ✅ Verify that the person is actually associated with ${org}. Through email domain, Linkedin or other research methods.
+          website: ${orgWebsite}
+          customers served: ${customers}
         ✅ Make sure to assign this person to the correct organization which should be ➡️ ${org}.
         ✅ Respond to ${email} when the all three steps are completed and inform them that they are now allowed to login to their dashboard.
          `,
