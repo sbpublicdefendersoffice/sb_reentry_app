6660f6ad9e19a905612ec46a47267f0eebeab4c6
Bryan Burzon <bburzon@countyofsb.org>
2022-04-06 17:56:23 -0700

    WIP

---
+++ components/FreshStartLanding/FreshStartHowToApply/FreshStartHowToApply.tsx
@@ -0,0 +1,162 @@
+import { Button, Paper, Typography } from '@mui/material'
+import NextLink from 'next/link'
+import useLanguage from '../../../hooks/useLanguage'
+import type { CopyHolder } from '../../../types/'
+import { Paragraph } from '../../../ui'
+import styles from './FreshStartHowToApply.module.css'
+
+interface Location {
+  href: string
+  name_english: string
+  name_spanish: string
+}
+
+const locations: Location[] = [
+  {
+    href: 'https://www.google.com/maps/place/1100+Anacapa+St,+Santa+Barbara,+CA+93101/@34.4242013,-119.7045169,17z/',
+    name_english: 'Our Santa Barbara Location',
+    name_spanish: 'Nuestra ubicación en Santa Bárbara',
+  },
+  {
+    href: 'https://www.google.com/maps/place/312+E+Cook+St,+Santa+Maria,+CA+93454/@34.9492102,-120.434486,17z/',
+    name_english: 'Our Santa Maria Location',
+    name_spanish: 'Nuestra ubicación en Santa María',
+  },
+  {
+    href: 'https://www.google.com/maps/place/115+Civic+Center+Plaza,+Lompoc,+CA+93436/@34.637649,-120.4553627,17z/',
+    name_english: 'Our Lompoc Location',
+    name_spanish: 'Nuestra ubicación en Lompoc',
+  },
+]
+
+const copy: CopyHolder = {
+  english: {
+    pageTitle: 'Fresh Start',
+    fill: "Fill out one easy form and you'll be connected to the Santa Barbara Public Defender's Office to help with your petitions for record clearance",
+    login: 'Login',
+    freshStart: ' and apply for Fresh Start through the web portal',
+    howApply: 'How do I apply?',
+    call: "Call the Public Defender's Office at ",
+    download: 'Download the ',
+    intake: 'Intake',
+    and: ' and ',
+    finance: 'Financial Declaration',
+    forms: ' Fill them out and email them to Amanda Paison at ',
+    fax: ' fax them to (805) 568-3564, ',
+    drop: 'or drop them off at:',
+    recognize:
+      'We recognize that different options work for different people. With this is mind, we have multiple ways to apply for record expungement',
+  },
+  spanish: {
+    pageTitle: 'Fresh Start',
+    fill: 'Complete un formulario fácil y se conectara con la Oficina del Defensor Publico de Santa Barbara para ayudarlo con sus peticiones de autorización de registros.',
+    login: 'Inicie sesión',
+    freshStart: ' y solicite Nuevo Comienzo a través del portal web',
+    howApply: '¿Cómo me inscribo?',
+    call: 'Llame a la Oficina del Defensor Publico al ',
+    download: 'Descarga los formularios de ',
+    intake: 'admisión',
+    and: ' y ',
+    finance: 'declaración financiera',
+    forms: 'Complétalos y envíalos por correo electrónico a  Amanda Paison a ',
+    fax: ' enviar por fax (805) 614-6735 ',
+    drop: 'o dejarlos en:',
+    recognize:
+      'Reconocemos que diferentes opciones funcionan para diferentes personas. Teniendo esto en cuenta, tenemos varias formas de solicitar la eliminación de antecedentes penales.',
+  },
+}
+
+const FreshStartHowToApply = () => {
+  const { language } = useLanguage()
+  const {
+    pageTitle,
+    login,
+    call,
+    freshStart,
+    download,
+    recognize,
+    howApply,
+    intake,
+    and,
+    finance,
+    forms,
+    fax,
+    drop,
+  } = copy[language]
+  return (
+    <div className={styles.HowToApply}>
+      <Paragraph size="large-text">{howApply}</Paragraph>
+      <br />
+      <Paragraph size="med-text">{recognize}</Paragraph>
+      <div className={styles.Cards}>
+        <Paper className={styles.Card} elevation={3}>
+          <Typography variant="h5">{login + freshStart}</Typography>
+          <NextLink href="/login">
+            <Button
+              className={styles.ApplyButton}
+              variant="contained"
+              size="large"
+            >
+              {pageTitle + ' ' + login}
+            </Button>
+          </NextLink>
+        </Paper>
+        <Paper className={styles.Card} elevation={3}>
+          <Typography variant="h5">{download}</Typography>
+          <Typography variant="h5">{intake + and + finance}</Typography>
+          <NextLink href="/documents/expungementintakeform">
+            <Button
+              className={styles.ApplyButton}
+              variant="contained"
+              size="large"
+            >
+              {intake}
+            </Button>
+          </NextLink>
+          <NextLink href="/documents/expungementfinanceform">
+            <Button
+              className={styles.ApplyButton}
+              variant="contained"
+              size="large"
+            >
+              {finance}
+            </Button>
+          </NextLink>
+        </Paper>
+        <Paper className={styles.Card} elevation={3}>
+          <Typography variant="h5">
+            {call}
+            <br />
+            <br />
+            <a href="tel:8056991444">(805) 699-1444</a>
+          </Typography>
+        </Paper>
+      </div>
+      <Paragraph size="med-text">
+        {forms}
+        <a href="mailto:apaison@countyofsb.org" target="_blank">
+          apaison@countyofsb.org
+        </a>
+        {fax + drop}
+      </Paragraph>
+      <br />
+      <Paragraph size="med-text" className={styles.ListItem}>
+        <ul>
+          {locations.map(loc => {
+            const { href } = loc
+
+            return (
+              <li key={href} className={styles.ListItem}>
+                <a href={href} target="_blank">
+                  {loc[`name_${language}`]}
+                </a>
+              </li>
+            )
+          })}
+        </ul>
+      </Paragraph>
+    </div>
+  )
+}
+
+export default FreshStartHowToApply
--- components/FreshStartLanding/FreshStartLanding.tsx
+++ components/FreshStartLanding/FreshStartLanding.tsx
@@ -1,111 +1,47 @@
-import { Title, Paragraph } from '../../ui'
+import useLanguage from '../../hooks/useLanguage'
+import type { CopyHolder } from '../../types/'
+import { Paragraph, Title } from '../../ui'
 import LetUsHelpHeading from '../LetUsHelpHeading'
 import PictureWithOval from '../PictureWithOval'
+import FreshStartHowToApply from './FreshStartHowToApply/FreshStartHowToApply'
 import styles from './FreshStartLanding.module.css'
-
-import type { CopyHolder } from '../../types/'
-import useLanguage from '../../hooks/useLanguage'
+import FreshStartServices from './FreshStartServices/FreshStartServices'
 
 const copy: CopyHolder = {
   english: {
-    reduce: 'Reduce or dismiss your record in Santa Barbara county',
-    fill: "Fill out one easy form and you'll be connected to the Santa Barbara Public Defender's Office to help with your petitions for record clearance",
-    login: 'Login',
-    apply: ' and apply today',
+    pageTitle: 'Fresh Start',
     info1: `A criminal record can make finding employment, obtaining housing, enrolling in and funding an education, and obtaining/retaining legal permanent residence or citizenship status very difficult.`,
     info2: `The good news is that there are options for you to move forward, even if you have made mistakes in the past, and the Public Defender’s Office would be happy to help you get off to a Fresh Start! Our staff will review your criminal history and seek all appropriate methods of relief for you.`,
     info3: `In order to best advise you on your record relief options, we will obtain a copy of your California Department of Justice Criminal History Report and potentially other court records on your behalf at no cost to you.`,
-    list1: `Expungement (Dismissal) of Criminal Convictions`,
-    list2: `Prop. 47 Relief (Reclassifying Certain Low Level Felonies to Misdemeanors)`,
-    list3: `Prop. 64 Relief (Reclassifying Certain Marijuana Crimes)`,
-    list4: `Motions to Reduce Felony Convictions to Misdemeanors`,
-    list5: `Motions for Early Termination of Probation`,
-    list6: `Human Trafficking Victim Conviction Relief`,
-    list7: `Sealing Adult Arrest Records`,
-    list8: `Sealing Adult Arrest Records for the Factually Innocent`,
-    list9: `Certificates of Rehabilitation/Gubernatorial Pardons`,
-    list10: `Correcting Errors in Department of Justice Criminal History Records`,
-    list11: `DNA Database Expungement`,
-    list12: `Gang Documentation Relief`,
-    list13: `Dismissal Petitions for Successful PC1000 Participants with Collateral Consequences per PC1203.43`,
   },
   spanish: {
-    reduce: 'Reducir o descartar su registro en el condado de Santa Bárbara',
-    fill: 'Complete un formulario fácil y se conectara con la Oficina del Defensor Publico de Santa Barbara para ayudarlo con sus peticiones de autorización de registros.',
-    login: 'Inicie sesión',
-    apply: ' y solicite hoy',
+    pageTitle: 'Fresh Start',
     info1: `Un registro de antecedentes penales puede hacer encontrar empleo, obtención de vivienda, matricularse y financiar una educación, y obteniendo/conservar la residencia permanente legal o estado de ciudadanía muy difícil.`,
     info2: `Las buena noticia es que hay opciones para que sigas adelante, aunque hayas cometido errores en el pasado, y la Oficina del Defensor Público estará encantados de ayudarlo a tener un Nuevo Comienzo. Nuestro personal revisara su historial criminal y busque todos los métodos apropiados de alivio para usted.`,
     info3: `Para poder asesorarte mejor sobre sus opciones de alivio de registros, obtendremos una copia de su Informe de antecedentes penales del Departamento de Justicia de California y potencialmente otros registros judiciales en su nombre sin costo alguno para usted.`,
-    list1: `Eliminación (Destitución) de condenas penales`,
-    list2: `Alivio de la Prop. 47 (Reclasificación de ciertos delitos graves de bajo nivel a delitos menores)`,
-    list3: `Alivio de la Prop. 64 (Reclasificación de ciertos delitos relacionados con la marihuana)`,
-    list4: `Mociones para reducir las condenas por delitos graves a delitos menores`,
-    list5: `Moción para la terminación anticipada de la libertad condicional`,
-    list6: `Alivio de condena de victimas de trata de personas`,
-    list7: `Sellado de registros de arresto de adultos`,
-    list8: `Sellado de registros de arresto de adultos para personas inocentes`,
-    list9: `Certificados de Rehabilitación/Indultos de Gobernador`,
-    list10: `Corrección de errores en los registros de antecedentes penales de Departamento de Justicia`,
-    list11: `Borrado de la base de datos de ADN`,
-    list12: `Alivio de documentación de pandillas`,
-    list13: `Peticiones de despido para participantes exitosos de PC 1000 con consecuencias colaterales de PC1203.43`,
   },
 }
 
 const FreshStartLanding = () => {
   const { language } = useLanguage()
-  const {
-    info1,
-    info2,
-    info3,
-    fill,
-    login,
-    apply,
-    list1,
-    list2,
-    list3,
-    list4,
-    list5,
-    list6,
-    list7,
-    list8,
-    list9,
-    list10,
-    list11,
-    list12,
-    list13,
-  } = copy[language]
+  const { pageTitle, info1, info2, info3 } = copy[language]
   return (
     <div className={styles.Container}>
       <div className={styles.Header}>
-        <PictureWithOval color="peri" pic="resourcesWomenPic.jpg" />
-        <div className={styles.HeaderText}>
-          <Title>Fresh Start</Title>
-          <Paragraph size="heading-text" className={styles.TextBlock}>
-            {info1}
-          </Paragraph>
-          <Paragraph size="heading-text" className={styles.TextBlock}>
-            {info2}
-          </Paragraph>
-          <Paragraph size="heading-text" className={styles.TextBlock}>
-            {info3}
-          </Paragraph>
-        </div>
+        <PictureWithOval color="peri" pic="familyFriendsPic.jpg" />
+        <LetUsHelpHeading>
+          <Title>{pageTitle}</Title>
+          <Paragraph size="med-text">{info1}</Paragraph>
+          <br />
+          <Paragraph size="med-text">{info2}</Paragraph>
+          <br />
+          <Paragraph size="med-text">{info3}</Paragraph>
+        </LetUsHelpHeading>
       </div>
+      <FreshStartHowToApply></FreshStartHowToApply>
+      <FreshStartServices></FreshStartServices>
     </div>
   )
 }
 
 export default FreshStartLanding
-
-{
-  /* <div className={styles.Container}>
-      <div className={styles.Header}>
-        <PictureWithOval color="peri" pic="resourcesWomenPic.jpg" />
-        <div className={styles.HeaderText}>
-          <Title>Fresh Start</Title>
-        </div>
-      </div>
-    </div> */
-}
