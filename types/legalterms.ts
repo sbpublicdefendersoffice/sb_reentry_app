7accb01160aa9710990bd4b6540649c7b4ffb7d8
Bryan Burzon <bburzon@countyofsb.org>
2022-05-23 14:50:57 -0700

    wip: update translation

--- types/legalterms.ts
+++ types/legalterms.ts
@@ -6,7 +6,7 @@ export const legaltermsCopy: CopyHolder = {
       'You might hear these terms while in court. You can always ask your Public Defender, Social Worker or anyone else for further clarification. Here is a great place to start',
   },
   spanish: {
-    title: 'Términos que puede escuchar',
+    title: 'Terminos que puede escuchar',
     description:
       'Es posible que escuche estos términos en la corte. Siempre puede pedirle más aclaraciones a su defensor público, trabajador social o cualquier otra persona. Este es un gran lugar para comenzar.',
   },
